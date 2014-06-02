74af7462330cdf55a34b08995ce338e44931719b
John Nowak <johnnowak@nypl.org>
2014-06-02 18:48:24 -0400

    Started implementation of OPDS parsing

---
+++ Simplified/NYPLOPDSAcquisitionFeed.m
@@ -0,0 +1,27 @@
+#import "NSDate+NYPLDateAdditions.h"
+
+#import "NYPLOPDSAcquisitionFeed.h"
+
+@interface NYPLOPDSAcquisitionFeed ()
+
+@property (nonatomic) NSString *identifier;
+@property (nonatomic) NSString *title;
+@property (nonatomic) NSDate *updated;
+
+@end
+
+@implementation NYPLOPDSAcquisitionFeed
+
+- (id)initWithDocument:(SMXMLDocument *)document
+{
+  self = [super init];
+  if(!self) return nil;
+  
+  self.identifier = [document.root childNamed:@"identifier"].value;
+  self.title = [document.root childNamed:@"title"].value;
+  self.updated = [NSDate dateWithRFC3339:[document.root childNamed:@"date"].value];
+
+  return self;
+}
+
+@end
