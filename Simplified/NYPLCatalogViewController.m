3677ad9f415a3ad8fa244e75a8da1506f72b6b7a
Winnie Quinn <me@winniequinn.com>
2014-06-09 16:22:10 -0400

    Added fetching of main navigation feed

--- Simplified/NYPLCatalogViewController.m
+++ Simplified/NYPLCatalogViewController.m
@@ -1,4 +1,12 @@
+#import "NYPLConfiguration.h"
+
 #import "NYPLCatalogViewController.h"
+
+@interface NYPLCatalogViewController ()
+
+@property (nonatomic, retain) UIActivityIndicatorView *activityIndicatorView;
+
+@end
 
 @implementation NYPLCatalogViewController
 
@@ -12,6 +20,37 @@
   self.title = @"All Books";
   
   return self;
+}
+
+- (void)loadNavigationFeed
+{
+  [self.activityIndicatorView startAnimating];
+  [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+  
+  [[[NSURLSession sharedSession]
+    dataTaskWithURL:[NYPLConfiguration mainFeedURL]
+    completionHandler:^(NSData *const data, NSURLResponse *const response, NSError *const error) {
+      NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+      [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        [self.activityIndicatorView stopAnimating];
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+      }];
+    }]
+   resume];
+}
+
+#pragma mark UIViewController
+
+- (void)viewDidLoad
+{
+  self.view.backgroundColor = [UIColor whiteColor];
+  
+  self.activityIndicatorView = [[UIActivityIndicatorView alloc]
+                                initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+  self.activityIndicatorView.center = self.view.center;
+  [self.view addSubview:self.activityIndicatorView];
+  
+  [self loadNavigationFeed];
 }
 
 @end
