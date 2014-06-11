bfd4d9ba00d4cd5a67b62c03e33627de677b9a06
Winnie Quinn <me@winniequinn.com>
2014-06-11 16:05:09 -0400

    Added NYPLURLSetSession

---
+++ Simplified/NYPLURLSetSession.m
@@ -0,0 +1,54 @@
+#import "NYPLURLSetSession.h"
+
+@interface NYPLURLSetSession ()
+
+@property (nonatomic, retain) NSURLSession *session;
+
+@end
+
+@implementation NYPLURLSetSession
+
+- (id)initWithURLSet:(NSSet *const)urls
+   completionHandler:(void (^)(NSDictionary *dataDictionary))handler
+{
+  self = [super init];
+  if(!self) return nil;
+  
+  if(!urls || !handler) {
+    @throw NSInvalidArgumentException;
+  }
+  
+  // TODO: This should be handled more intelligently.
+  self.session = [NSURLSession sharedSession];
+  
+  NSLock *const lock = [[NSLock alloc] init];
+  NSMutableDictionary *const results = [NSMutableDictionary dictionaryWithCapacity:[urls count]];
+  __block NSUInteger tasksRemaining = [urls count];
+  
+  for(NSURL *const url in urls) {
+    [[self.session
+      dataTaskWithRequest:[NSURLRequest requestWithURL:url]
+      completionHandler:^(NSData *const data,
+                          __attribute__((unused)) NSURLResponse *response,
+                          NSError *const error) {
+        [lock lock];
+        if(error) {
+          [results setObject:error forKey:url];
+        } else {
+          [results setObject:data forKey:url];
+        }
+        --tasksRemaining;
+        BOOL done = !tasksRemaining;
+        [lock unlock];
+        
+        if(done) {
+          handler(results);
+        }
+      }]
+     resume];
+  }
+  
+  return self;
+}
+
+@end
