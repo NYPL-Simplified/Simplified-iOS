85a6dbec98bd23bd3580013136539a3684b2fd92
John Nowak <johnnowak@nypl.org>
2014-06-11 16:05:09 -0400

    Added NYPLURLSetSession

---
+++ Simplified/NYPLURLSetSession.h
@@ -0,0 +1,12 @@
+@import Foundation;
+
+@interface NYPLURLSetSession : NSObject
+
+// |handler| will be called with a dictionary containing the URLs as keys and NSData objects as
+// values. If any errors occurred, an NSError object will be present where an NSData object would
+// have otherwise been.
+- (id)initWithURLSet:(NSSet *)urls
+   completionHandler:(void (^)(NSDictionary *dataDictionary))handler;
+
+
+@end
