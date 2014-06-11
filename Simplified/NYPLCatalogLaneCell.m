997eb53d5b033d395dc2b5d3c45d29e1c5346ff1
Winnie Quinn <me@winniequinn.com>
2014-06-11 11:14:41 -0400

    Added NYPLCatalogLaneCell to test target

--- Simplified/NYPLCatalogLaneCell.m
+++ Simplified/NYPLCatalogLaneCell.m
@@ -1,3 +1,5 @@
+#import "NYPLOPDSLink.h"
+
 #import "NYPLCatalogLaneCell.h"
 
 @implementation NYPLCatalogLaneCell
@@ -10,6 +12,21 @@
   UILabel *const titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, 200, 20)];
   titleLabel.text = entry.title;
   [self addSubview:titleLabel];
+
+  NSUInteger index = [entry.links indexOfObjectPassingTest:
+                      ^BOOL(id const obj, __attribute__((unused)) NSUInteger i, BOOL *stop) {
+                        NSString *rel = ((NYPLOPDSLink *) obj).rel;
+                        if([rel isEqualToString:@"http://opds-spec.org/recommended"]) {
+                          *stop = YES;
+                          return YES;
+                        }
+                        return NO;
+                      }];
+  
+  if(index != NSNotFound) {
+    NSURL *url = [NSURL URLWithString:entry.links[index]];
+    NSURLSessionDataTask *task = ...
+  }
   
   return self;
 }
