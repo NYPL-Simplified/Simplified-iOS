74081854b515c693b4e112ca2b37175f6600bf3f
John Nowak <johnnowak@nypl.org>
2014-06-11 17:16:44 -0400

    Implemented very rough first pass at catalog lane image layout

--- Simplified/NYPLURLSetSession.h
+++ Simplified/NYPLURLSetSession.h
@@ -2,6 +2,7 @@
 
 @interface NYPLURLSetSession : NSObject
 
+// designated initializer
 // |handler| will be called with a dictionary containing the URLs as keys and NSData objects as
 // values. If any errors occurred, an NSError object will be present where an NSData object would
 // have otherwise been.
