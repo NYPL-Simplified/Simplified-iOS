53fe69c62df012c1d62e3b3e09b817b439e9beaf
Winnie Quinn <me@winniequinn.com>
2014-06-13 15:25:57 -0400

    Basis for new NYPLCatalogLaneCell

---
+++ Simplified/NYPLCatalogLaneCellOld.h
@@ -0,0 +1,10 @@
+@import UIKit;
+
+#include "NYPLOPDSEntry.h"
+
+@interface NYPLCatalogLaneCellOld : UITableViewCell
+
+// designated initializer
+- (id)initWithEntry:(NYPLOPDSEntry *)entry;
+
+@end
