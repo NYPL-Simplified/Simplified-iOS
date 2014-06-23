c0a71ab2cb42bbc89741d18edd506b5b655cb9a6
Winnie Quinn <me@winniequinn.com>
2014-06-23 12:06:49 -0400

    Added stub for NYPLCatalogCategory

---
+++ Simplified/NYPLCatalogCategory.m
@@ -0,0 +1,40 @@
+#import "NYPLAsyncData.h"
+
+#import "NYPLCatalogCategory.h"
+
+@interface NYPLCatalogCategory ()
+
+@property (nonatomic) NSArray *books;
+@property (nonatomic) NSString *title;
+
+@end
+
+@implementation NYPLCatalogCategory
+
++ (void)withURL:(NSURL *)url handler:(void (^)(NYPLCatalogCategory *category))handler
+{
+  [NYPLAsyncData
+   withURL:url
+   completionHandler:^(__attribute__((unused)) NSData *const data) {
+     // TODO
+     handler(nil);
+   }];
+}
+
+- (id)initWithBooks:(NSArray *const)books
+              title:(NSString *const)title
+{
+  self = [super init];
+  if(!self) return nil;
+  
+  if(!(books && title)) {
+    @throw NSInvalidArgumentException;
+  }
+  
+  self.books = books;
+  self.title = title;
+  
+  return self;
+}
+
+@end
