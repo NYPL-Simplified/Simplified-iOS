f200bd2d32e1f08a78c0d0ac8885bcac9dbb5d4e
John Nowak <johnnowak@nypl.org>
2014-06-25 17:06:14 -0400

    Began implementation of book display for categories

---
+++ Simplified/NYPLCatalogCategoryCell.h
@@ -0,0 +1,3 @@
+@interface NYPLCatalogCategoryCell : UICollectionViewCell
+
+@end
--- Simplified/NYPLCatalogCategoryViewController.m
+++ Simplified/NYPLCatalogCategoryViewController.m
@@ -1,14 +1,20 @@
+#import "NYPLCatalogBook.h"
 #import "NYPLCatalogCategory.h"
+#import "NYPLCatalogCategoryCell.h"
 
 #import "NYPLCatalogCategoryViewController.h"
 
 @interface NYPLCatalogCategoryViewController ()
+  <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property (nonatomic) UIActivityIndicatorView *activityIndicatorView;
 @property (nonatomic) NYPLCatalogCategory *category;
+@property (nonatomic) UICollectionView *collectionView;
 @property (nonatomic) NSURL *url;
 
 @end
+
+static NSString *const reuseIdentifier = @"NYPLCatalogCategoryViewControllerCell";
 
 @implementation NYPLCatalogCategoryViewController
 
@@ -30,11 +36,24 @@
 
 - (void)viewDidLoad
 {
+  self.collectionView = [[UICollectionView alloc]
+                         initWithFrame:self.view.bounds
+                         collectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+  self.collectionView.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
+                                          UIViewAutoresizingFlexibleHeight);
+  self.collectionView.dataSource = self;
+  self.collectionView.delegate = self;
+  [self.collectionView registerClass:[NYPLCatalogCategoryCell class]
+          forCellWithReuseIdentifier:reuseIdentifier];
+  self.collectionView.backgroundColor = [UIColor whiteColor];
+  self.collectionView.hidden = YES;
+  [self.view addSubview:self.collectionView];
+  
   self.activityIndicatorView = [[UIActivityIndicatorView alloc]
                                 initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-  self.activityIndicatorView.center = self.view.center;
   [self.activityIndicatorView startAnimating];
   [self.view addSubview:self.activityIndicatorView];
+  [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
   
   [NYPLCatalogCategory
    withURL:self.url
@@ -42,6 +61,7 @@
      [[NSOperationQueue mainQueue] addOperationWithBlock:^{
        self.activityIndicatorView.hidden = YES;
        [self.activityIndicatorView stopAnimating];
+       [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
        
        if(!category) {
          [[[UIAlertView alloc]
@@ -62,11 +82,95 @@
    }];
 }
 
+- (void)viewWillLayoutSubviews
+{
+  self.activityIndicatorView.center = self.view.center;
+}
+
+-(void)willRotateToInterfaceOrientation:(__attribute__((unused)) UIInterfaceOrientation)orientation
+                               duration:(__attribute__((unused)) NSTimeInterval)duration
+{
+  [self.collectionView.collectionViewLayout invalidateLayout];
+}
+
+#pragma mark UICollectionViewDataSource
+
+- (NSInteger)collectionView:(__attribute__((unused)) UICollectionView *)collectionView
+     numberOfItemsInSection:(__attribute__((unused)) NSInteger)section
+{
+  return self.category.books.count;
+}
+
+// TODO: This test method needs to be replaced with one that returns the correct cell.
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+  UICollectionViewCell *const cell = [collectionView
+                                      dequeueReusableCellWithReuseIdentifier:reuseIdentifier
+                                      forIndexPath:indexPath];
+  
+  UILabel *const label = [[UILabel alloc] init];
+  label.text = ((NYPLCatalogBook *) self.category.books[indexPath.row]).title;
+  label.frame = cell.contentView.bounds;
+  for(UIView *const view in [[cell contentView] subviews]) {
+    [view removeFromSuperview];
+  }
+  [cell.contentView addSubview:label];
+  
+  cell.backgroundColor = [UIColor colorWithHue:([label.text hash] / (CGFloat) ULONG_MAX)
+                                    saturation:1.0
+                                    brightness:1.0
+                                         alpha:1.0];
+  
+  return cell;
+}
+
+#pragma mark UICollectionViewDelegateFlowLayout
+
+- (UIEdgeInsets)collectionView:(__attribute__((unused)) UICollectionView *)collectionView
+                        layout:(__attribute__((unused)) UICollectionViewLayout*)collectionViewLayout
+        insetForSectionAtIndex:(__attribute__((unused)) NSInteger)section
+{
+  return UIEdgeInsetsZero;
+}
+
+- (CGFloat)collectionView:(__attribute__((unused)) UICollectionView *)collectionView
+                   layout:(__attribute__((unused)) UICollectionViewLayout*)collectionViewLayout
+minimumInteritemSpacingForSectionAtIndex:(__attribute__((unused)) NSInteger)section
+{
+  return 0.0;
+}
+
+- (CGFloat)collectionView:(__attribute__((unused)) UICollectionView *)collectionView
+                   layout:(__attribute__((unused)) UICollectionViewLayout*)collectionViewLayout
+minimumLineSpacingForSectionAtIndex:(__attribute__((unused)) NSInteger)section
+{
+  return 0.0;
+}
+
+- (CGSize)collectionView:(__attribute__((unused)) UICollectionView *)collectionView
+                  layout:(__attribute__((unused)) UICollectionViewLayout*)collectionViewLayout
+  sizeForItemAtIndexPath:(__attribute__((unused)) NSIndexPath *)indexPath
+{
+  // TODO: Sizes need to be set properly.
+  switch(self.interfaceOrientation) {
+    case UIInterfaceOrientationPortrait:
+      // fallthrough
+    case UIInterfaceOrientationPortraitUpsideDown:
+      return CGSizeMake(384, 120);
+    case UIInterfaceOrientationLandscapeLeft:
+      // fallthrough
+    case UIInterfaceOrientationLandscapeRight:
+      return CGSizeMake(341, 120);
+  }
+}
+
 #pragma mark -
 
 - (void)didLoadCategory
 {
-  
+  [self.collectionView reloadData];
+  self.collectionView.hidden = NO;
 }
 
 @end
