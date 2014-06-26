bc5e623ab435e3473a7f8c91fd8b58f62f427c48
Winnie Quinn <me@winniequinn.com>
2014-06-26 18:05:35 -0400

    Added covers to categories

--- Simplified/NYPLCatalogCategoryCell.m
+++ Simplified/NYPLCatalogCategoryCell.m
@@ -1,8 +1,11 @@
+#import "NYPLCoverSession.h"
+
 #import "NYPLCatalogCategoryCell.h"
 
 @interface NYPLCatalogCategoryCell ()
 
 @property (nonatomic) UILabel *author;
+@property (nonatomic) UIImageView *cover;
 @property (nonatomic) UILabel *title;
 
 @end
@@ -13,16 +16,18 @@
 
 - (void)layoutSubviews
 {
+  self.cover.frame = CGRectMake(5, 5, 90, self.frame.size.height - 10);
+  
   [self.title sizeToFit];
   CGRect titleFrame = self.title.frame;
-  titleFrame.origin = CGPointMake(5, 5);
-  titleFrame.size.width = self.frame.size.width - 10;
+  titleFrame.origin = CGPointMake(100, 5);
+  titleFrame.size.width = self.frame.size.width - 105;
   self.title.frame = titleFrame;
   
   [self.author sizeToFit];
   CGRect authorFrame = self.author.frame;
-  authorFrame.origin = CGPointMake(5, titleFrame.origin.y + titleFrame.size.height + 5);
-  authorFrame.size.width = self.frame.size.width - 10;
+  authorFrame.origin = CGPointMake(100, titleFrame.origin.y + titleFrame.size.height + 5);
+  authorFrame.size.width = self.frame.size.width - 105;
   self.author.frame = authorFrame;
 }
 
@@ -31,17 +36,36 @@
 - (void)setBook:(NYPLCatalogBook *const)book
 {
   if(!self.author) {
-    self.author = [[UILabel alloc] init];
+    self.author = [[UILabel alloc] initWithFrame:CGRectZero];
     [self addSubview:self.author];
   }
   
+  if(!self.cover) {
+    self.cover = [[UIImageView alloc] initWithFrame:CGRectZero];
+    [self addSubview:self.cover];
+  }
+  
   if(!self.title) {
-    self.title = [[UILabel alloc] init];
+    self.title = [[UILabel alloc] initWithFrame:CGRectZero];
     [self addSubview:self.title];
   }
   
   self.author.text = [book.authorStrings componentsJoinedByString:@"; "];
+  self.cover.image = nil;
   self.title.text = book.title;
+  
+  self.cover.image = [[NYPLCoverSession sharedSession] cachedImageForURL:book.imageURL];
+  
+  if(!self.cover.image) {
+    [[NYPLCoverSession sharedSession]
+     withURL:book.imageURL
+     completionHandler:^(UIImage *const image) {
+       [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+         self.cover.image = image;
+         [self.cover sizeToFit];
+       }];
+     }];
+  }
   
   [self setNeedsLayout];
 }
