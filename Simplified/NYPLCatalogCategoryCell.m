04701fe472806fe4474b37a4fb31d738ff256f27
John Nowak <johnnowak@nypl.org>
2014-06-26 16:26:54 -0400

    Added proper display of title and author using NYPLCatalogCategoryCell

--- Simplified/NYPLCatalogCategoryCell.m
+++ Simplified/NYPLCatalogCategoryCell.m
@@ -1,5 +1,49 @@
 #import "NYPLCatalogCategoryCell.h"
 
+@interface NYPLCatalogCategoryCell ()
+
+@property (nonatomic) UILabel *author;
+@property (nonatomic) UILabel *title;
+
+@end
+
 @implementation NYPLCatalogCategoryCell
+
+#pragma mark UIView
+
+- (void)layoutSubviews
+{
+  [self.title sizeToFit];
+  CGRect titleFrame = self.title.frame;
+  titleFrame.origin = CGPointMake(5, 5);
+  titleFrame.size.width = self.frame.size.width - 10;
+  self.title.frame = titleFrame;
+  
+  [self.author sizeToFit];
+  CGRect authorFrame = self.author.frame;
+  authorFrame.origin = CGPointMake(5, titleFrame.origin.y + titleFrame.size.height + 5);
+  authorFrame.size.width = self.frame.size.width - 10;
+  self.author.frame = authorFrame;
+}
+
+#pragma mark -
+
+- (void)setBook:(NYPLCatalogBook *const)book
+{
+  if(!self.author) {
+    self.author = [[UILabel alloc] init];
+    [self addSubview:self.author];
+  }
+  
+  if(!self.title) {
+    self.title = [[UILabel alloc] init];
+    [self addSubview:self.title];
+  }
+  
+  self.author.text = [book.authorStrings componentsJoinedByString:@"; "];
+  self.title.text = book.title;
+  
+  [self setNeedsLayout];
+}
 
 @end
