713ada721d9f860b906a4439ae8b37bebbf35862
Winnie Quinn <me@winniequinn.com>
2014-06-26 17:12:51 -0400

    Added NYPLCoverSession

---
+++ Simplified/NYPLCoverSession.m
@@ -0,0 +1,66 @@
+#import "NYPLCoverSession.h"
+
+@interface NYPLCoverSession ()
+
+@property (nonatomic) NSURLSession *session;
+
+@end
+
+static NSUInteger const diskCacheInMegabytes = 20;
+static NSUInteger const memoryCacheInMegabytes = 2;
+
+static NYPLCoverSession *sharedCoverSession = nil;
+
+@implementation NYPLCoverSession
+
++ (NYPLCoverSession *)sharedSession
+{
+  static dispatch_once_t predicate;
+  
+  dispatch_once(&predicate, ^{
+    sharedCoverSession = [[NYPLCoverSession alloc] init];
+    if(!sharedCoverSession) {
+      NYPLLOG(@"Failed to create shared session.");
+    }
+  });
+  
+  return sharedCoverSession;
+}
+
+#pragma mark NSObject
+
+- (instancetype)init
+{
+  if(sharedCoverSession) {
+    @throw NSGenericException;
+  }
+  
+  self = [super init];
+  if(!self) return nil;
+  
+  NSURLSessionConfiguration *const configuration =
+    [NSURLSessionConfiguration defaultSessionConfiguration];
+  
+  assert(configuration.URLCache);
+  
+  configuration.URLCache.diskCapacity = 1024 * 1024 * diskCacheInMegabytes;
+  configuration.URLCache.memoryCapacity = 1024 * 1024 * memoryCacheInMegabytes;
+  
+  self.session = [NSURLSession sessionWithConfiguration:configuration];
+  
+  return self;
+}
+
+- (void)withURL:(NSURL *const)url completionHandler:(void (^)(UIImage *image))handler
+{
+  [[self.session
+    dataTaskWithURL:url
+    completionHandler:^(NSData *const data,
+                        __attribute__((unused)) NSURLResponse *response,
+                        __attribute__((unused)) NSError *const error) {
+      handler([UIImage imageWithData:data]);
+    }]
+   resume];
+}
+  
+@end
