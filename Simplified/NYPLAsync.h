0cd5197300cfc75e75421b6a8d60e07b153ada13
Winnie Quinn <me@winniequinn.com>
2014-06-27 16:24:31 -0400

    Removed remaining non-trivial dependencies on old NYPLAsync functions

--- Simplified/NYPLAsync.h
+++ Simplified/NYPLAsync.h
@@ -1,12 +1,3 @@
 // Using this function is equivalent to using |dispatch_async| with the default global priority
 // queue.
 void NYPLAsyncDispatch(dispatch_block_t block);
-
-// |data| will be |nil| if an error occurred.
-// The handler is guaranteed to be called on the main thread.
-void NYPLAsyncFetch(NSURL *url, void (^ handler)(NSData *data));
-
-// The handler will be called with a dictionary containing all input URLs as keys.
-// Each key will be associated with an NSData value if successful, else an NSNull value.
-// The handler is guaranteed to be called on the main thread.
-void NYPLAsyncFetchSet(NSSet *set, void (^ handler)(NSDictionary *dataDictionary));
--- Simplified/NYPLSession.h
+++ Simplified/NYPLSession.h
@@ -4,6 +4,8 @@
 
 - (void)withURL:(NSURL *)url completionHandler:(void (^)(NSData *data))handler;
 
+- (void)withURLs:(NSSet *)urls handler:(void (^)(NSDictionary *dataDictionary))handler;
+
 - (NSData *)cachedDataForURL:(NSURL *)url;
 
 @end
