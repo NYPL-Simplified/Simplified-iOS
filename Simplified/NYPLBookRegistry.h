8badf6eebc9d3a7e8a79e47422f43d550043d77e
Winnie Quinn <me@winniequinn.com>
2014-07-07 14:44:17 -0400

    Added basic methods to NYPLContentRegistry (now NYPLBookRegistry)

---
+++ Simplified/NYPLBookRegistry.h
@@ -0,0 +1,25 @@
+// All methods of this class declared below are thread-safe.
+
+#import "NYPLBook.h"
+
+@interface NYPLBookRegistry : NSObject
+
++ (NYPLBookRegistry *)sharedInstance;
+
+// This adds a book to the book registry until it is manually removed. It allows the application to
+// present information about obtained books when offline. Attempting to add a book already present
+// will overwrite the existing book as if |updateBook:| were called.
+- (void)addBook:(NYPLBook *)book;
+
+// This method should be called whenever new book information is retreived from a server. Doing so
+// ensures that once the user has seen the new information, they will continue to do so when
+// accessing the application off-line or when viewing books outside of the catalog. Attempts to
+// update a book not already stored in the registry will simply be ignored, so it's reasonable to
+// call this method whenever new information is obtained regardless of a given book's state.
+- (void)updateBook:(NYPLBook *)book;
+
+// Given an identifier, this method removes a book from the registry. Attempting to remove a book
+// that is not present will result in an error being logged.
+- (void)removeBookForIdentifier:(NSString *const)book;
+
+@end
