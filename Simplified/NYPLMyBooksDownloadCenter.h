c69f7ba7489ea77bc64379cd14aee6f41bcb3a35
Winnie Quinn <me@winniequinn.com>
2014-07-15 19:58:00 -0400

    Added downloading and related notifications

--- Simplified/NYPLMyBooksDownloadCenter.h
+++ Simplified/NYPLMyBooksDownloadCenter.h
@@ -1,5 +1,21 @@
 #import "NYPLBook.h"
 
+typedef NS_ENUM(NSInteger, NYPLMyBooksDownloadCenterStatus) {
+  NYPLMyBooksDownloadCenterStatusDownloading,
+  NYPLMyBooksDownloadCenterStatusFailed,
+  NYPLMyBooksDownloadCenterStatusSucceeded
+};
+
+// book     : NYPLBook
+// progress : double                          (range 0..1, boxed as NSNumber)
+// status   : NYPLMyBooksDownloadCenterStatus (boxed as NSNumber)
+static NSString *const NYPLMyBooksDownloadCenterNotification =
+  @"NYPLMyBooksDownloadCenterNotification";
+
+static NSString *const NYPLMyBooksDownloadCenterNotificationBookKey = @"book";
+static NSString *const NYPLMyBooksDownloadCenterNotificationProgressKey = @"progress";
+static NSString *const NYPLMyBooksDownloadCenterNotificationStatusKey = @"status";
+
 @interface NYPLMyBooksDownloadCenter : NSObject
 
 + (NYPLMyBooksDownloadCenter *)sharedDownloadCenter;
