602bc6770b7cad9458584dbf89b5fcd468052daa
John Nowak <johnnowak@nypl.org>
2014-07-16 17:29:22 -0400

    Added barcode and PIN fields to settings

--- Simplified/NYPLSettingsViewController.m
+++ Simplified/NYPLSettingsViewController.m
@@ -1,5 +1,12 @@
 #import "NYPLSettingsViewController.h"
 
+@interface NYPLSettingsViewController ()
+
+@property (nonatomic) UITextField *barcodeField;
+@property (nonatomic) UITextField *PINField;
+
+@end
+
 @implementation NYPLSettingsViewController
 
 #pragma mark NSObject
@@ -18,7 +25,35 @@
 
 - (void)viewDidLoad
 {
-  self.view.backgroundColor = [UIColor whiteColor];
+  self.view.backgroundColor = [UIColor lightGrayColor];
+  
+  self.barcodeField = [[UITextField alloc] initWithFrame:CGRectMake(5, 69, 310, 31)];
+  self.barcodeField.backgroundColor = [UIColor whiteColor];
+  self.barcodeField.placeholder = @"Barcode";
+  [self.barcodeField addTarget:self
+                        action:@selector(fieldsDidChange)
+              forControlEvents:UIControlEventEditingDidEnd];
+  [self.view addSubview:self.barcodeField];
+  
+  self.PINField = [[UITextField alloc] initWithFrame:CGRectMake(5, 69 + 31 + 5, 310, 31)];
+  self.PINField.backgroundColor = [UIColor whiteColor];
+  self.PINField.placeholder = @"PIN";
+  [self.PINField addTarget:self
+                    action:@selector(fieldsDidChange)
+          forControlEvents:UIControlEventEditingDidEnd];
+  [self.view addSubview:self.PINField];
+}
+
+- (void)viewWillAppear:(__attribute__((unused)) BOOL)animated
+{
+  
+}
+
+#pragma mark -
+
+- (void)fieldsDidChange
+{
+  NSLog(@"%@ : %@", self.barcodeField.text, self.PINField.text);
 }
 
 @end
