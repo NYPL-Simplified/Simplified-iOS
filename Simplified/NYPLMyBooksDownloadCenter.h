68c966c669ba58fca3e14b55eac0141c0d4e6d9f
Winnie Quinn <me@winniequinn.com>
2014-07-18 15:12:43 -0400

    Stated partial plan for how download flow should work

--- Simplified/NYPLMyBooksDownloadCenter.h
+++ Simplified/NYPLMyBooksDownloadCenter.h
@@ -7,8 +7,13 @@ static NSString *const NYPLMyBooksDownloadCenterDidChange =
 
 + (NYPLMyBooksDownloadCenter *)sharedDownloadCenter;
 
+// This method should be called iff a barcode and PIN are already set. If they're not, the part of
+// the app that wants to initiate a download is responsible for collecting that information first.
+// If it fails to do so and just starts the download anyway, the user will get a download error and
+// have to resume the download through My Books (which is less than ideal).
 - (void)startDownloadForBook:(NYPLBook *)book;
 
+// The value returned is in the range [0.0, 1.0].
 - (double)downloadProgressForBookIdentifier:(NSString *)bookIdentifier;
 
 @end
