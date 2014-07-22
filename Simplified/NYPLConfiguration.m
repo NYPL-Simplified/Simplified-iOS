45f2fa7cbdd231cee9fa72b82d0f351a58b6420d
Winnie Quinn <me@winniequinn.com>
2014-07-22 16:56:59 -0400

    Implemented simpler, generic approach to tint and font faces

--- Simplified/NYPLConfiguration.m
+++ Simplified/NYPLConfiguration.m
@@ -2,29 +2,6 @@
 
 @implementation NYPLConfiguration
 
-+ (void)initialize
-{
-  [[UIButton appearance] setTintColor:[self mainColor]];
-  
-  [[UINavigationBar appearance]
-   setTitleTextAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Bold"
-                                                                 size:18.0]}];
-  
-  [[UINavigationBar appearance] setTintColor:[self mainColor]];
-  
-  [[UIBarButtonItem appearance]
-   setTitleTextAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Medium"
-                                                                 size:18.0]}
-   forState:UIControlStateNormal];
-  
-  [[UITabBarItem appearance]
-   setTitleTextAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Medium"
-                                                                 size:12.0]}
-   forState:UIControlStateNormal];
-  
-  [[UITabBar appearance] setTintColor:[self mainColor]];
-}
-
 + (NSURL *)mainFeedURL
 {
   return [NSURL URLWithString:@"http://library-simplified.herokuapp.com"];
@@ -34,6 +11,16 @@
 + (UIColor *)mainColor
 {
   return [UIColor colorWithRed:240/255.0 green:115/255.0 blue:31/255.0 alpha:1.0];
+}
+
++ (NSString *)systemFontName
+{
+  return @"AvenirNext-Medium";
+}
+
++ (NSString *)boldSystemFontName
+{
+  return @"AvenirNext-Bold";
 }
 
 @end
