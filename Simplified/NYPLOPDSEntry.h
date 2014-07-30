7d1513bd53529b0475f2f78b59c76a57eb7ec79e
Winnie Quinn <me@winniequinn.com>
2014-07-30 17:46:07 -0400

    Added summaries to OPDS entries and books

--- Simplified/NYPLOPDSEntry.h
+++ Simplified/NYPLOPDSEntry.h
@@ -5,6 +5,7 @@
 @property (nonatomic, readonly) NSArray *authorStrings;
 @property (nonatomic, readonly) NSString *identifier;
 @property (nonatomic, readonly) NSArray *links;
+@property (nonatomic, readonly) NSString *summary; // nilable
 @property (nonatomic, readonly) NSString *title;
 @property (nonatomic, readonly) NSDate *updated;
 
