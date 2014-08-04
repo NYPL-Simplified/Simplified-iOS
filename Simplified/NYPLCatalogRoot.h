80e47b8f7e1a35e52c7c544de246bcbcdd514c48
John Nowak <johnnowak@nypl.org>
2014-08-04 17:56:27 -0400

    Added OpenSearch description retrieval for NYPLCatalogRoot

--- Simplified/NYPLCatalogRoot.h
+++ Simplified/NYPLCatalogRoot.h
@@ -1,13 +1,13 @@
 @interface NYPLCatalogRoot : NSObject
 
 @property (nonatomic, readonly) NSArray *lanes;
-@property (nonatomic, readonly) NSURL *openSearchURL; // nilable
+@property (nonatomic, readonly) NSString *searchTemplate; // nilable
 
 // In the callback, |root| will be |nil| if an error occurred.
 + (void)withURL:(NSURL *)URL handler:(void (^)(NYPLCatalogRoot *root))handler;
 
 // designated initializer
 - (instancetype)initWithLanes:(NSArray *)lanes
-                openSearchURL:(NSURL *)openSearchURL;
+               searchTemplate:(NSString *)searchTemplate;
 
 @end
