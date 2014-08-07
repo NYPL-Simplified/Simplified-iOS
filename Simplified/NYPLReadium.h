38718e700e7162479de5eab8e37a38993ecbe710
Winnie Quinn <me@winniequinn.com>
2014-08-07 14:34:34 -0400

    Integrated RDServices project

---
+++ Simplified/NYPLReadium.h
@@ -0,0 +1,12 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wauto-import"
+#pragma clang diagnostic ignored "-Wobjc-interface-ivars"
+
+#import "RDContainer.h"
+#import "RDMediaOverlaysSmilModel.h"
+#import "RDNavigationElement.h"
+#import "RDPackage.h"
+#import "RDSmilData.h"
+#import "RDSpineItem.h"
+
+#pragma clang diagnostic pop
