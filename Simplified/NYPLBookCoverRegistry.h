01a321522b06727b7c160f951cfd5f62c178bf30
John Nowak <johnnowak@nypl.org>
2014-09-15 15:56:20 -0400

    Partial progress on NYPLBookCoverRegistry

--- Simplified/NYPLBookCoverRegistry.h
+++ Simplified/NYPLBookCoverRegistry.h
@@ -1,8 +1,15 @@
+@class NYPLBook;
+
 @interface NYPLBookCoverRegistry : NSObject
 
 + (id)new NS_UNAVAILABLE;
 - (id)init NS_UNAVAILABLE;
 
 + (NYPLBookCoverRegistry *)sharedRegistry;
+
+// NOTE: Handlers are *not* called on the main thread.
+
+// |image| will be nil if a cover could not be obtained.
+- (void)temporaryThumbnailImageForBook:(NYPLBook *)book handler:(void (^)(UIImage *image))handler;
 
 @end
--- Simplified/NYPLBookCoverRegistry.m
+++ Simplified/NYPLBookCoverRegistry.m
@@ -1,4 +1,13 @@
+#import "NYPLAsync.h"
+#import "NYPLBook.h"
+
 #import "NYPLBookCoverRegistry.h"
+
+@interface NYPLBookCoverRegistry ()
+
+@property (nonatomic) NSURLSession *session;
+
+@end
 
 @implementation NYPLBookCoverRegistry
 
@@ -15,6 +24,41 @@
   });
   
   return sharedRegistry;
+}
+
+#pragma mark NSObject
+
+- (instancetype)init
+{
+  self = [super init];
+  if(!self) return nil;
+  
+  NSURLSessionConfiguration *const configuration =
+    [NSURLSessionConfiguration defaultSessionConfiguration];
+  
+  configuration.HTTPCookieStorage = nil;
+  configuration.HTTPMaximumConnectionsPerHost = 8;
+  configuration.HTTPShouldUsePipelining = YES;
+  configuration.timeoutIntervalForRequest = 5.0;
+  configuration.timeoutIntervalForResource = 20.0;
+  configuration.URLCredentialStorage = nil;
+  
+  self.session = [NSURLSession sessionWithConfiguration:configuration];
+  
+  return self;
+}
+
+#pragma mark -
+
+- (void)temporaryThumbnailImageForBook:(NYPLBook *)book handler:(void (^)(UIImage *image))handler
+{
+  if(!book.imageThumbnailURL) {
+    NYPLAsyncDispatch(^{
+      handler(nil);
+    });
+  }
+  
+  // TODO
 }
 
 @end
