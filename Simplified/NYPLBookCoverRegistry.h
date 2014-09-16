f79e48db009ea7d9e24d0e51674599a6983367fa
Winnie Quinn <me@winniequinn.com>
2014-09-16 13:44:16 -0400

    Implemented temporary cover methods for NYPLBookCoverRegistry

--- Simplified/NYPLBookCoverRegistry.h
+++ Simplified/NYPLBookCoverRegistry.h
@@ -7,9 +7,16 @@
 
 + (NYPLBookCoverRegistry *)sharedRegistry;
 
-// NOTE: Handlers are *not* called on the main thread.
+// All handlers are called on the main thread.
 
 // |image| will be nil if a cover could not be obtained.
-- (void)temporaryThumbnailImageForBook:(NYPLBook *)book handler:(void (^)(UIImage *image))handler;
+- (void)temporaryThumbnailImageForBook:(NYPLBook *)book
+                               handler:(void (^)(UIImage *image))handler;
+
+// The set passed in should contain NYPLBook objects. If |books| is nil or does not strictly contain
+// NYPLBook objects, the handler will be called with nil. Otherwise, the dictionary passed to the
+// handler maps book identifiers to images (or nulls in case of error).
+- (void)temporaryThumbnailImagesForBooks:(NSSet *)books
+                                 handler:(void (^)(NSDictionary *dictionary))handler;
 
 @end
--- Simplified/NYPLBookCoverRegistry.m
+++ Simplified/NYPLBookCoverRegistry.m
@@ -1,5 +1,5 @@
-#import "NYPLAsync.h"
 #import "NYPLBook.h"
+#import "NYPLNull.h"
 
 #import "NYPLBookCoverRegistry.h"
 
@@ -8,6 +8,9 @@
 @property (nonatomic) NSURLSession *session;
 
 @end
+
+static NSUInteger const diskCacheInMegabytes = 16;
+static NSUInteger const memoryCacheInMegabytes = 2;
 
 @implementation NYPLBookCoverRegistry
 
@@ -41,6 +44,8 @@
   configuration.HTTPShouldUsePipelining = YES;
   configuration.timeoutIntervalForRequest = 5.0;
   configuration.timeoutIntervalForResource = 20.0;
+  configuration.URLCache.diskCapacity = 1024 * 1024 * diskCacheInMegabytes;
+  configuration.URLCache.memoryCapacity = 1024 * 1024 * memoryCacheInMegabytes;
   configuration.URLCredentialStorage = nil;
   
   self.session = [NSURLSession sessionWithConfiguration:configuration];
@@ -53,12 +58,76 @@
 - (void)temporaryThumbnailImageForBook:(NYPLBook *)book handler:(void (^)(UIImage *image))handler
 {
   if(!book.imageThumbnailURL) {
-    NYPLAsyncDispatch(^{
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
       handler(nil);
-    });
+    }];
+    return;
   }
   
-  // TODO
+  [[self.session
+    dataTaskWithRequest:[NSURLRequest requestWithURL:book.imageThumbnailURL]
+    completionHandler:^(NSData *const data,
+                        __attribute__((unused)) NSURLResponse *response,
+                        __attribute__((unused)) NSError *error) {
+      [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        handler([UIImage imageWithData:data]);
+      }];
+    }]
+   resume];
+}
+
+- (void)temporaryThumbnailImagesForBooks:(NSSet *)books
+                                 handler:(void (^)(NSDictionary *dictionary))handler
+{
+  if(!books) {
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+      handler(nil);
+    }];
+    return;
+  }
+  
+  if(!books.count) {
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+      handler(^{});
+    }];
+    return;
+  }
+  
+  for(id const object in books) {
+    if(![object isKindOfClass:[NYPLBook class]]) {
+      [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        handler(nil);
+      }];
+      return;
+    }
+  }
+  
+  NSLock *const lock = [[NSLock alloc] init];
+  NSMutableDictionary *const dictionary = [NSMutableDictionary dictionary];
+  __block NSUInteger remaining = books.count;
+  
+  for(NYPLBook *const book in books) {
+    if(!book.imageThumbnailURL) {
+      dictionary[book.identifier] = [NSNull null];
+      continue;
+    }
+    [[self.session
+      dataTaskWithRequest:[NSURLRequest requestWithURL:book.imageThumbnailURL]
+      completionHandler:^(NSData *const data,
+                          __attribute__((unused)) NSURLResponse *response,
+                          __attribute__((unused)) NSError *error) {
+        [lock lock];
+        dictionary[book.identifier] = NYPLNullFromNil([UIImage imageWithData:data]);
+        --remaining;
+        if(!remaining) {
+          [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            handler(dictionary);
+          }];
+        }
+        [lock unlock];
+      }]
+     resume];
+  }
 }
 
 @end
