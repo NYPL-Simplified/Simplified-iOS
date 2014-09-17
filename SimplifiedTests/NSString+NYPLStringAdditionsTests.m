ff7d362ed2ae271d88330ec28c9e660960465a2a
John Nowak <johnnowak@nypl.org>
2014-09-17 14:05:11 -0400

    Added tests for NSString+NYPLStringAdditions and fixed padding bug

---
+++ SimplifiedTests/NSString+NYPLStringAdditionsTests.m
@@ -0,0 +1,27 @@
+#import "NSString+NYPLStringAdditions.h"
+
+#import <XCTest/XCTest.h>
+
+@interface NSString_NYPLStringAdditionsTests : XCTestCase
+
+@end
+
+@implementation NSString_NYPLStringAdditionsTests
+
+- (void)testEncode
+{
+  NSString *const s = [@"ynJZEsWMnTudEGg646Tmua"
+                       fileSystemSafeBase64EncodedStringUsingEncoding:NSUTF8StringEncoding];
+  
+  XCTAssertEqualObjects(s, @"eW5KWkVzV01uVHVkRUdnNjQ2VG11YQ");
+}
+
+- (void)testDecode
+{
+  NSString *const s = [@"eW5KWkVzV01uVHVkRUdnNjQ2VG11YQ"
+                       fileSystemSafeBase64DecodedStringUsingEncoding:NSUTF8StringEncoding];
+  
+  XCTAssertEqualObjects(s, @"ynJZEsWMnTudEGg646Tmua");
+}
+
+@end
