0121a70af05015ad82973fe9c6ff507cfd76c090
John Nowak <johnnowak@nypl.org>
2014-10-02 12:18:36 -0400

    Added contentVerticalAlignment property to NYPLLinearView

--- Simplified/NYPLLinearView.h
+++ Simplified/NYPLLinearView.h
@@ -1,5 +1,12 @@
+typedef NS_ENUM(NSInteger, NYPLLinearViewContentVerticalAlignment) {
+  NYPLLinearViewContentVerticalAlignmentTop,
+  NYPLLinearViewContentVerticalAlignmentMiddle,
+  NYPLLinearViewContentVerticalAlignmentBottom
+};
+
 @interface NYPLLinearView : UIView
 
+@property (nonatomic) NYPLLinearViewContentVerticalAlignment contentVerticalAlignment;
 @property (nonatomic) CGFloat padding;
 
 @end
