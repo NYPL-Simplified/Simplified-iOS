bcb314581b32328baa0096e3e4c3d4a676109117
John Nowak <johnnowak@nypl.org>
2014-10-15 11:49:49 -0400

    Added NYPLCatalogFacet, related tests, and other related changes

--- Simplified/NYPLCatalogFacet.h
+++ Simplified/NYPLCatalogFacet.h
@@ -1,8 +1,13 @@
+@class NYPLOPDSLink;
+
 @interface NYPLCatalogFacet : NSObject
 
 @property (nonatomic, readonly) BOOL active;
 @property (nonatomic, readonly) NSURL *href;
-@property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) NSString *title; // nilable
+
+// The link provided must have the |NYPLOPDSRelationFacet| relation.
++ (NYPLCatalogFacet *)catalogFacetWithLink:(NYPLOPDSLink *)link;
 
 - (instancetype)initWithActive:(BOOL)active
                           href:(NSURL *)href
