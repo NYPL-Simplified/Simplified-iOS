b4302d77325ec0cbee89c5fcb259f929a56428f2
John Nowak <johnnowak@nypl.org>
2014-10-18 16:45:09 -0400

    Added semi-functional tap-to-turn functionality
    This only works until the iframe used by Readium changes.

--- Simplified/Simplified.js
+++ Simplified/Simplified.js
@@ -1,1 +1,19 @@
-alert("Loaded!");
+(function() {
+  
+  function handleTouchEnd(event) {
+    var touch = event.changedTouches[0];
+    var position = touch.clientX / document.documentElement.clientWidth;
+    if(position <= 0.15) {
+      window.location = "simplified:tap-back"
+    } else if(position >= 0.85) {
+      window.location = "simplified:tap-forward"
+    }
+  }
+
+  // handles border between inner content and edge of screen
+  document.addEventListener("touchend", handleTouchEnd, false);
+  
+  // handles inner content
+  window.frames["epubContentIframe"].document.addEventListener("touchend", handleTouchEnd, false);
+
+})();
