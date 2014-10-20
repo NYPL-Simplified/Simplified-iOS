85eb9902f9b875b830d53e44eabd696a8c680be8
Winnie Quinn <me@winniequinn.com>
2014-10-20 13:50:40 -0400

    Ignore swipes and only turn pages on tap

--- Simplified/Simplified.js
+++ Simplified/Simplified.js
@@ -1,7 +1,21 @@
 function Simplified() {
   
+  var startX = 0;
+  var startY = 0;
+  
+  var handleTouchStart = function(event) {
+    var touch = event.changedTouches[0];
+    startX = touch.screenX;
+    startY = touch.screenY;
+  };
+  
   var handleTouchEnd = function(event) {
     var touch = event.changedTouches[0];
+    if(Math.abs(touch.screenX - startX) >= 5 ||
+       Math.abs(touch.screenY - startY) >= 5) {
+      // This is not a simple tap.
+      return;
+    }
     var position = touch.screenX / screen.width;
     if(position <= 0.2) {
       window.location = "simplified:tap-back";
@@ -13,11 +27,14 @@ function Simplified() {
   };
   
   // Handles border between inner content and edge of screen.
+  document.addEventListener("touchstart", handleTouchStart, false);
   document.addEventListener("touchend", handleTouchEnd, false);
   
   // Handles inner content. Should be called each time the page changes as the iframe may have
   // changed.
   this.pageDidChange = function() {
+    window.frames["epubContentIframe"].removeEventListener("touchstart", handleTouchStart);
+    window.frames["epubContentIframe"].addEventListener("touchstart", handleTouchStart, false);
     window.frames["epubContentIframe"].removeEventListener("touchend", handleTouchEnd);
     window.frames["epubContentIframe"].addEventListener("touchend", handleTouchEnd, false);
   };
