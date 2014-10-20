f03ec9de0c1d1664004749cfbc34b528202d7a22
John Nowak <johnnowak@nypl.org>
2014-10-20 14:07:44 -0400

    Added swipe-to-turn

--- Simplified/Simplified.js
+++ Simplified/Simplified.js
@@ -11,19 +11,35 @@ function Simplified() {
   
   var handleTouchEnd = function(event) {
     var touch = event.changedTouches[0];
-    if(Math.abs(touch.screenX - startX) >= 5 ||
-       Math.abs(touch.screenY - startY) >= 5) {
-      // This is not a simple tap.
+    
+    // Tap to turn.
+    if(Math.abs(touch.screenX - startX) <= 5 &&
+       Math.abs(touch.screenY - startY) <= 5) {
+      var position = touch.screenX / screen.width;
+      if(position <= 0.2) {
+        window.location = "simplified:tap-back";
+      } else if(position >= 0.8) {
+        window.location = "simplified:tap-forward";
+      }
+      event.stopPropagation();
+      event.preventDefault();
       return;
     }
-    var position = touch.screenX / screen.width;
-    if(position <= 0.2) {
-      window.location = "simplified:tap-back";
-    } else if(position >= 0.8) {
-      window.location = "simplified:tap-forward";
+    
+    var relativeDistanceX = (touch.screenX - startX) / screen.width;
+    var slope = (touch.screenY - startY) / (touch.screenX - startX);
+    
+    // Swipe to turn.
+    if(Math.abs(slope) <= 0.5 && Math.abs(relativeDistanceX) >= 0.1) {
+      if(relativeDistanceX > 0) {
+        window.location = "simplified:tap-back";
+      } else {
+        window.location = "simplified:tap-forward";
+      }
+      event.stopPropagation();
+      event.preventDefault();
+      return;
     }
-    event.stopPropagation();
-    event.preventDefault();
   };
   
   // Handles border between inner content and edge of screen.
