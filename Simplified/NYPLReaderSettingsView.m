fd819cdf9acbc881c1ad8508c398e8162b327a51
Winnie Quinn <me@winniequinn.com>
2014-11-12 17:37:36 -0500

    Added sans-serif and serif buttons to reader settings

--- Simplified/NYPLReaderSettingsView.m
+++ Simplified/NYPLReaderSettingsView.m
@@ -1,4 +1,13 @@
+#import "NYPLConfiguration.h"
+
 #import "NYPLReaderSettingsView.h"
+
+@interface NYPLReaderSettingsView ()
+
+@property (nonatomic) UIButton *sansButton;
+@property (nonatomic) UIButton *serifButton;
+
+@end
 
 @implementation NYPLReaderSettingsView
 
@@ -12,6 +21,26 @@
   self.backgroundColor = [UIColor lightGrayColor];
   
   [self sizeToFit];
+  
+  self.sansButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.sansButton.backgroundColor = [NYPLConfiguration backgroundColor];
+  [self.sansButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+  [self.sansButton setTitle:@"Aa" forState:UIControlStateNormal];
+  self.sansButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:24];
+  [self.sansButton addTarget:self
+                      action:@selector(didSelectSans)
+            forControlEvents:UIControlEventTouchUpInside];
+  [self addSubview:self.sansButton];
+  
+  self.serifButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.serifButton.backgroundColor = [NYPLConfiguration backgroundColor];
+  [self.serifButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+  [self.serifButton setTitle:@"Aa" forState:UIControlStateNormal];
+  self.serifButton.titleLabel.font = [UIFont fontWithName:@"Georgia" size:24];
+  [self.sansButton addTarget:self
+                      action:@selector(didSelectSerif)
+            forControlEvents:UIControlEventTouchUpInside];
+  [self addSubview:self.serifButton];
   
   [[NSNotificationCenter defaultCenter]
    addObserverForName:UIScreenBrightnessDidChangeNotification
@@ -33,6 +62,19 @@
 
 #pragma mark UIView
 
+- (void)layoutSubviews
+{
+  self.sansButton.frame = CGRectMake(0,
+                                     0,
+                                     CGRectGetWidth(self.frame) / 2.0,
+                                     CGRectGetHeight(self.frame) / 4.0);
+  
+  self.serifButton.frame = CGRectMake(CGRectGetWidth(self.frame) / 2.0,
+                                      0,
+                                      CGRectGetWidth(self.frame) / 2.0,
+                                      CGRectGetHeight(self.frame) / 4.0);
+}
+
 - (CGSize)sizeThatFits:(CGSize)size
 {
   CGFloat const w = 320;
@@ -43,6 +85,22 @@
   }
   
   return CGSizeMake(w > size.width ? size.width : w, h > size.height ? size.height : h);
+}
+
+#pragma mark -
+
+- (void)didSelectSans
+{
+  self.fontType = NYPLReaderSettingsViewFontTypeSans;
+  
+  [self.delegate readerSettingsView:self didSelectFontType:self.fontType];
+}
+
+- (void)didSelectSerif
+{
+  self.fontType = NYPLReaderSettingsViewFontTypeSerif;
+  
+  [self.delegate readerSettingsView:self didSelectFontType:self.fontType];
 }
 
 @end
