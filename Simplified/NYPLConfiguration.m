8aebe515ff85eecddf7b8712e48896d7d5e3e828
John Nowak <johnnowak@nypl.org>
2014-11-17 18:04:57 -0500

    Added brightness assets and finished control layout

--- Simplified/NYPLConfiguration.m
+++ Simplified/NYPLConfiguration.m
@@ -33,6 +33,16 @@
   return [UIColor colorWithWhite:250/255.0 alpha:1.0];
 }
 
++ (UIColor *)backgroundDarkColor
+{
+  return [UIColor colorWithWhite:5/255.0 alpha:1.0];
+}
+
++ (UIColor *)backgroundSepiaColor
+{
+  return [UIColor colorWithRed:255/255.0 green:245/255.0 blue:210/255.0 alpha:1.0];
+}
+
 + (NSString *)systemFontName
 {
   return @"AvenirNext-Medium";
