384cf1e7eeb31e0c594a8ed91a9b3c5eed3373d7
John Nowak <johnnowak@nypl.org>
2014-12-08 16:50:41 -0500

    Added use of generated covers

--- Simplified/NYPLMyBooksCoverRegistry.h
+++ Simplified/NYPLMyBooksCoverRegistry.h
@@ -9,17 +9,17 @@
 
 // All handlers are called on the main thread.
 
-// |image| will be nil if a cover could not be obtained.
 - (void)thumbnailImageForBook:(NYPLBook *)book
                       handler:(void (^)(UIImage *image))handler;
 
 // The set passed in should contain NYPLBook objects. If |books| is nil or does not strictly contain
 // NYPLBook objects, the handler will be called with nil. Otherwise, the dictionary passed to the
-// handler maps book identifiers to images (or nulls in case of error).
+// handler maps book identifiers to images.
 - (void)thumbnailImagesForBooks:(NSSet *)books
-                        handler:(void (^)(NSDictionary *bookIdentifersToImagesAndNulls))handler;
+                        handler:(void (^)(NSDictionary *bookIdentifersToImages))handler;
 
-// Immediately returns the cached thumbnail if available, else nil.
+// Immediately returns the cached thumbnail if available, else nil. Generated images are not
+// returned.
 - (UIImage *)cachedThumbnailImageForBook:(NYPLBook *)book;
 
 // Pinned images will remain on-disk until they are manually unpinned. Only pinned images are
