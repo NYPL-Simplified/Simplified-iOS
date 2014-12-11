02cfe59921f2d4b3173aad3a8c0abaf957000355
Winnie Quinn <me@winniequinn.com>
2014-12-11 15:58:13 -0500

    Adding missing nilable property comments for NYPLOPDSLink
    Closes #89

--- Simplified/NYPLOPDSLink.h
+++ Simplified/NYPLOPDSLink.h
@@ -4,11 +4,11 @@
 
 @property (nonatomic, readonly) NSDictionary *attributes;
 @property (nonatomic, readonly) NSURL *href;
-@property (nonatomic, readonly) NSString *rel;
-@property (nonatomic, readonly) NSString *type;
-@property (nonatomic, readonly) NSString *hreflang;
-@property (nonatomic, readonly) NSString *title;
-@property (nonatomic, readonly) NSString *length;
+@property (nonatomic, readonly) NSString *rel; // nilable
+@property (nonatomic, readonly) NSString *type; // nilable
+@property (nonatomic, readonly) NSString *hreflang; // nilable
+@property (nonatomic, readonly) NSString *title; // nilable
+@property (nonatomic, readonly) NSString *length; // nilable
 
 + (id)new NS_UNAVAILABLE;
 - (id)init NS_UNAVAILABLE;
