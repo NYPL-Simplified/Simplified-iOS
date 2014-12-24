686ac7e973358414654049fc44c93828247b39b4
Winnie Quinn <me@winniequinn.com>
2014-12-23 19:39:09 -0500

    Further factoring of functionality into NYPLReaderReadiumView

--- Simplified/NYPLReaderView.h
+++ Simplified/NYPLReaderView.h
@@ -1,5 +1,12 @@
+typedef NS_ENUM(NSInteger, NYPLReaderViewGesture) {
+  NYPLReaderViewGestureLeft,
+  NYPLReaderViewGestureCenter,
+  NYPLReaderViewGestureRight
+};
+
 @protocol NYPLReaderView
 
 @property (nonatomic, readonly) BOOL bookIsCorrupt;
+@property (nonatomic, readonly) BOOL loaded;
 
 @end
--- Simplified/NYPLReaderViewDelegate.h
+++ Simplified/NYPLReaderViewDelegate.h
@@ -1,7 +1,9 @@
-@protocol NYPLReaderView;
+#import "NYPLReaderView.h"
 
 @protocol NYPLReaderViewDelegate
 
 - (void)readerView:(id<NYPLReaderView>)readerView didEncounterCorruptionForBook:(NYPLBook *)book;
+- (void)readerView:(id<NYPLReaderView>)readerView didReceiveGesture:(NYPLReaderViewGesture)gesture;
+- (void)readerViewDidFinishLoading:(id<NYPLReaderView>)readerView;
 
 @end
