a99e6f89df65e2d143e4114912ad4a3ace90988f
Winnie Quinn <me@winniequinn.com>
2014-12-30 16:10:03 -0500

    Merged NYPLReaderViewDelegate into NYPLReaderView

--- Simplified/NYPLReaderViewDelegate.h
+++
@@ -1,9 +0,0 @@
-#import "NYPLReaderView.h"
-
-@protocol NYPLReaderViewDelegate
-
-- (void)readerView:(id<NYPLReaderView>)readerView didEncounterCorruptionForBook:(NYPLBook *)book;
-- (void)readerView:(id<NYPLReaderView>)readerView didReceiveGesture:(NYPLReaderViewGesture)gesture;
-- (void)readerViewDidFinishLoading:(id<NYPLReaderView>)readerView;
-
-@end
