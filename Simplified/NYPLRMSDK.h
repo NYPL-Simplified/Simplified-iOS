bfa335ed9b20e4fac6da07036850095d6ee6da5c
Winnie Quinn <me@winniequinn.com>
2015-01-05 18:55:09 -0500

    Added setup of RMServices

---
+++ Simplified/NYPLRMSDK.h
@@ -0,0 +1,8 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wauto-import"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wobjc-interface-ivars"
+
+#import "RMServices.h"
+
+#pragma clang diagnostic pop
