aac09d1436ef4e9a8a73eb49a73041eac830b545
Winnie Quinn <me@winniequinn.com>
2015-01-22 12:17:30 -0500

    Changed NYPLBookLocation to store unstructured data

--- Simplified/NYPLBookLocation.h
+++ Simplified/NYPLBookLocation.h
@@ -1,13 +1,21 @@
+// Due to differences in how different renderers (e.g. Readium, RMSDK, et cetera) want to handle
+// location information, it is necessary to store location information in an unstructured manner.
+// When creating an instance of this class, |locationString| is the renderer-specific data and
+// |renderer| is a string that uniquely identifies the renderer that generated it. When loading a
+// location, renderers can inspect |renderer| to ensure the location string they're about to use is
+// compatible with their underlying systems.
+
 @interface NYPLBookLocation : NSObject
 
-@property (nonatomic, readonly) NSString *CFI; // nilable
-@property (nonatomic, readonly) NSString *idref;
+@property (nonatomic, readonly) NSString *locationString;
+@property (nonatomic, readonly) NSString *renderer;
 
 + (id)new NS_UNAVAILABLE;
 - (id)init NS_UNAVAILABLE;
 
 // designated initializer
-- (instancetype)initWithCFI:(NSString *)CFI idref:(NSString *)idref;
+- (instancetype)initWithLocationString:(NSString *)locationString
+                              renderer:(NSString *)renderer;
 
 // designated initializer
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
--- Simplified/NYPLBookLocation.m
+++ Simplified/NYPLBookLocation.m
@@ -4,27 +4,28 @@
 
 @interface NYPLBookLocation ()
 
-@property (nonatomic) NSString *CFI;
-@property (nonatomic) NSString *idref;
+@property (nonatomic) NSString *locationString;
+@property (nonatomic) NSString *renderer;
 
 @end
 
-static NSString *const CFIKey = @"cfi";
-static NSString *const idrefKey = @"idref";
+static NSString *const locationStringKey = @"locationString";
+static NSString *const rendererKey = @"renderer";
 
 @implementation NYPLBookLocation
 
-- (instancetype)initWithCFI:(NSString *const)CFI idref:(NSString *const)idref
+- (instancetype)initWithLocationString:(NSString *const)locationString
+                              renderer:(NSString *const)renderer
 {
   self = [super init];
   if(!self) return nil;
  
-  if(!idref) {
+  if(!(locationString && renderer)) {
     @throw NSInvalidArgumentException;
   }
   
-  self.CFI = CFI;
-  self.idref = idref;
+  self.locationString = locationString;
+  self.renderer = renderer;
   
   return self;
 }
@@ -34,19 +35,19 @@ static NSString *const idrefKey = @"idref";
   self = [super init];
   if(!self) return nil;
   
-  self.CFI = NYPLNullToNil(dictionary[CFIKey]);
-  if(self.CFI && ![self.CFI isKindOfClass:[NSString class]]) return nil;
+  self.locationString = dictionary[locationStringKey];
+  if(![self.locationString isKindOfClass:[NSString class]]) return nil;
   
-  self.idref = dictionary[idrefKey];
-  if(![self.idref isKindOfClass:[NSString class]]) return nil;
+  self.renderer = dictionary[rendererKey];
+  if(![self.renderer isKindOfClass:[NSString class]]) return nil;
   
   return self;
 }
 
 - (NSDictionary *)dictionaryRepresentation
 {
-  return @{CFIKey: NYPLNullFromNil(self.CFI),
-           idrefKey: self.idref};
+  return @{locationStringKey: self.locationString,
+           rendererKey: self.renderer};
 }
 
 @end
