4f322a5f6fb67ee905dfcbcbf2c32bf0d864a7d8
Winnie Quinn <me@winniequinn.com>
2015-01-28 14:40:56 -0500

    Added NYPLSettings

---
+++ Simplified/NYPLSettings.m
@@ -0,0 +1,20 @@
+#import "NYPLSettings.h"
+
+@implementation NYPLSettings
+
++ (NYPLSettings *)sharedSettings
+{
+  static dispatch_once_t predicate;
+  static NYPLSettings *sharedSettings;
+  
+  dispatch_once(&predicate, ^{
+    sharedSettings = [[self alloc] init];
+    if(!sharedSettings) {
+      NYPLLOG(@"Failed to create shared settings.");
+    }
+  });
+  
+  return sharedSettings;
+}
+
+@end
