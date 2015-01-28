456aa245edf8d841809c417b7f694cb255ae51ee
Winnie Quinn <me@winniequinn.com>
2015-01-28 15:43:24 -0500

    Added NSUserDefaults-based settings for NYPLSettings

--- Simplified/NYPLSettings.h
+++ Simplified/NYPLSettings.h
@@ -1,4 +1,17 @@
+static NSString *const NYPLSettingsDidChangeNotification = @"NYPLSettingsDidChangeNotification";
+
+typedef NS_ENUM(NSInteger, NYPLSettingsRenderingEngine) {
+  NYPLSettingsRenderingEngineAutomatic,
+  NYPLSettingsRenderingEngineReadium,
+  NYPLSettingsRenderingEngineRMSDK10
+};
+
 @interface NYPLSettings : NSObject
+
+// Set to nil (the default) if no custom feed should be used.
+@property (atomic) NSURL *customMainFeedURL;
+
+@property (atomic) NYPLSettingsRenderingEngine renderingEngine;
 
 + (id)new NS_UNAVAILABLE;
 - (id)init NS_UNAVAILABLE;
