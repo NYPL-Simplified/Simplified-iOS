53dc4faa32f7739feed46cce77c573d8db1971ea
John Nowak <johnnowak@nypl.org>
2015-01-28 15:43:24 -0500

    Added NSUserDefaults-based settings for NYPLSettings

--- Simplified/NYPLSettings.m
+++ Simplified/NYPLSettings.m
@@ -1,5 +1,38 @@
 #import "NYPLSettings.h"
 
+static NSString *const customMainFeedURLKey = @"NYPLSettingsCustomMainFeedURL";
+
+static NSString *const renderingEngineKey = @"NYPLSettingsRenderingEngine";
+
+static NYPLSettingsRenderingEngine RenderingEngineFromString(NSString *const string)
+{
+  if(!string || [string isEqualToString:@"automatic"]) {
+    return NYPLSettingsRenderingEngineAutomatic;
+  }
+  
+  if([string isEqualToString:@"readium"]) {
+    return NYPLSettingsRenderingEngineReadium;
+  }
+  
+  if([string isEqualToString:@"rmsdk10"]) {
+    return NYPLSettingsRenderingEngineRMSDK10;
+  }
+  
+  @throw NSInvalidArgumentException;
+}
+
+static NSString *StringFromRenderingEngine(NYPLSettingsRenderingEngine const renderingEngine)
+{
+  switch(renderingEngine) {
+    case NYPLSettingsRenderingEngineAutomatic:
+      return @"automatic";
+    case NYPLSettingsRenderingEngineReadium:
+      return @"readium";
+    case NYPLSettingsRenderingEngineRMSDK10:
+      return @"rmsdk10";
+  }
+}
+
 @implementation NYPLSettings
 
 + (NYPLSettings *)sharedSettings
@@ -15,6 +48,28 @@
   });
   
   return sharedSettings;
+}
+
+- (NSURL *)customMainFeedURL
+{
+  return [[NSUserDefaults standardUserDefaults] URLForKey:customMainFeedURLKey];
+}
+
+- (void)setCustomMainFeedURL:(NSURL *const)customMainFeedURL
+{
+  [[NSUserDefaults standardUserDefaults] setURL:customMainFeedURL forKey:customMainFeedURLKey];
+}
+
+- (NYPLSettingsRenderingEngine)renderingEngine
+{
+  return RenderingEngineFromString([[NSUserDefaults standardUserDefaults]
+                                    stringForKey:renderingEngineKey]);
+}
+
+- (void)setRenderingEngine:(NYPLSettingsRenderingEngine const)renderingEngine
+{
+  [[NSUserDefaults standardUserDefaults] setObject:StringFromRenderingEngine(renderingEngine)
+                                            forKey:renderingEngineKey];
 }
 
 @end
