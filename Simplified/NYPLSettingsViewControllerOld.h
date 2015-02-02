6881a9176eef770013fb2664b11a47df0028e8ad
Winnie Quinn <me@winniequinn.com>
2015-02-02 15:30:30 -0500

    NYPLSettingsViewController -> NYPLSettingsViewControllerOld

--- Simplified/NYPLSettingsViewControllerOld.h
+++ Simplified/NYPLSettingsViewControllerOld.h
@@ -1,7 +1,7 @@
 // NOTE: This class is a placeholder for a real settings view controller. It exposes development-
 // only functionality that will not be in the release version. Localization is not supported.
 
-@interface NYPLSettingsViewController : UIViewController
+@interface NYPLSettingsViewControllerOld : UIViewController
 
 - (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
