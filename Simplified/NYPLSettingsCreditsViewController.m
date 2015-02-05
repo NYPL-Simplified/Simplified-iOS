70674891654eb226e957e46456edacd178c6a69f
Winnie Quinn <me@winniequinn.com>
2015-02-05 15:55:16 -0500

    Added stand-in credits

--- Simplified/NYPLSettingsCreditsViewController.m
+++ Simplified/NYPLSettingsCreditsViewController.m
@@ -1,6 +1,16 @@
+#import "NYPLConfiguration.h"
+
 #import "NYPLSettingsCreditsViewController.h"
 
+@interface NYPLSettingsCreditsViewController ()
+
+@property (nonatomic) UIWebView *webView;
+
+@end
+
 @implementation NYPLSettingsCreditsViewController
+
+#pragma mark NSObject
 
 - (instancetype)init
 {
@@ -12,9 +22,21 @@
   return self;
 }
 
+#pragma mark UIViewController
+
 - (void)viewDidLoad
 {
-  self.view.backgroundColor = [UIColor greenColor];
+  self.webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+  self.webView.autoresizingMask = (UIViewAutoresizingFlexibleHeight
+                                   | UIViewAutoresizingFlexibleWidth);
+  self.webView.backgroundColor = [NYPLConfiguration backgroundColor];
+  [self.webView loadRequest:
+   [NSURLRequest requestWithURL:
+    [NSURL fileURLWithPath:
+     [[NSBundle mainBundle]
+      pathForResource:@"credits"
+      ofType:@"html"]]]];
+  [self.view addSubview:self.webView];
 }
 
 @end
