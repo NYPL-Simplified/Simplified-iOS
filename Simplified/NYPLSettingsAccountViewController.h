0c9a75b23e00a980dda567bc401aff73b87a83ac
Winnie Quinn <me@winniequinn.com>
2015-02-11 18:14:22 -0500

    Added new Account section with barcode and revealable PIN

--- Simplified/NYPLSettingsAccountViewController.h
+++ Simplified/NYPLSettingsAccountViewController.h
@@ -1,4 +1,4 @@
-@interface NYPLSettingsAccountViewController : UIViewController
+@interface NYPLSettingsAccountViewController : UITableViewController
 
 - (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
