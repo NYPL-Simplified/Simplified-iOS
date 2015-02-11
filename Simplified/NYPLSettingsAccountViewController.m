02a5d5199ef8d577d9a1eac6aa9b2732b4550558
John Nowak <johnnowak@nypl.org>
2015-02-11 18:14:22 -0500

    Added new Account section with barcode and revealable PIN

--- Simplified/NYPLSettingsAccountViewController.m
+++ Simplified/NYPLSettingsAccountViewController.m
@@ -1,6 +1,43 @@
+#import "NYPLAccount.h"
 #import "NYPLConfiguration.h"
 
 #import "NYPLSettingsAccountViewController.h"
+
+typedef NS_ENUM(NSInteger, CellKind) {
+  CellKindBarcode,
+  CellKindPIN,
+  CellKindLoginLogout
+};
+
+static CellKind CellKindFromIndexPath(NSIndexPath *const indexPath)
+{
+  switch(indexPath.section) {
+    case 0:
+      switch(indexPath.row) {
+        case 0:
+          return CellKindBarcode;
+        case 1:
+          return CellKindPIN;
+        default:
+          @throw NSInvalidArgumentException;
+      }
+    case 1:
+      switch(indexPath.row) {
+        case 0:
+          return CellKindLoginLogout;
+        default:
+          @throw NSInvalidArgumentException;
+      }
+    default:
+      @throw NSInvalidArgumentException;
+  }
+}
+
+@interface NYPLSettingsAccountViewController ()
+
+@property (nonatomic) BOOL hiddenPIN;
+
+@end
 
 @implementation NYPLSettingsAccountViewController
 
@@ -8,10 +45,12 @@
 
 - (instancetype)init
 {
-  self = [super init];
+  self = [super initWithStyle:UITableViewStyleGrouped];
   if(!self) return nil;
   
   self.title = NSLocalizedString(@"Account", nil);
+  
+  self.tableView.allowsSelection = NO;
   
   return self;
 }
@@ -21,6 +60,96 @@
 - (void)viewDidLoad
 {
   self.view.backgroundColor = [NYPLConfiguration backgroundColor];
+}
+
+- (void)viewWillAppear:(__attribute__((unused)) BOOL)animated
+{
+  self.hiddenPIN = YES;
+  [self.tableView reloadData];
+}
+
+#pragma mark UITableViewDataSource
+
+- (UITableViewCell *)tableView:(__attribute__((unused)) UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *const)indexPath
+{
+  NSString *const barcode = [NYPLAccount sharedAccount].barcode;
+  NSString *const PIN = [NYPLAccount sharedAccount].PIN;
+  BOOL const loggedIn = [[NYPLAccount sharedAccount] hasBarcodeAndPIN];
+  
+  switch(CellKindFromIndexPath(indexPath)) {
+    case CellKindBarcode: {
+      UITableViewCell *const cell = [[UITableViewCell alloc]
+                                     initWithStyle:UITableViewCellStyleValue1
+                                     reuseIdentifier:nil];
+      cell.textLabel.text = NSLocalizedString(@"Barcode", nil);
+      if(loggedIn) {
+        cell.detailTextLabel.textAlignment = NSTextAlignmentLeft;
+        cell.detailTextLabel.text = barcode;
+      }
+      return cell;
+    }
+    case CellKindPIN: {
+      UITableViewCell *const cell = [[UITableViewCell alloc]
+                                     initWithStyle:UITableViewCellStyleValue1
+                                     reuseIdentifier:nil];
+      cell.textLabel.text = NSLocalizedString(@"PIN", nil);
+      if(loggedIn) {
+        cell.detailTextLabel.textAlignment = NSTextAlignmentLeft;
+        if(self.hiddenPIN) {
+          UIButton *const button = [UIButton buttonWithType:UIButtonTypeSystem];
+          button.titleLabel.font = [UIFont systemFontOfSize:17];
+          [button setTitle:@"Reveal" forState:UIControlStateNormal];
+          [button sizeToFit];
+          [button addTarget:self
+                     action:@selector(didSelectReveal)
+           forControlEvents:UIControlEventTouchUpInside];
+          cell.accessoryView = button;
+        } else {
+          cell.detailTextLabel.text = PIN;
+        }
+      }
+      return cell;
+    }
+    case CellKindLoginLogout: {
+      UITableViewCell *const cell = [[UITableViewCell alloc]
+                                     initWithStyle:UITableViewCellStyleValue1
+                                     reuseIdentifier:nil];
+      cell.textLabel.textColor = [NYPLConfiguration mainColor];
+      if(loggedIn) {
+        cell.textLabel.text = NSLocalizedString(@"LogOut", nil);
+      } else {
+        cell.textLabel.text = NSLocalizedString(@"LogIn", nil);
+      }
+      return cell;
+    }
+  }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(__attribute__((unused)) UITableView *)tableView
+{
+  return 2;
+}
+
+- (NSInteger)tableView:(__attribute__((unused)) UITableView *)tableView
+ numberOfRowsInSection:(NSInteger const)section
+{
+  switch(section) {
+    case 0:
+      return 2;
+    case 1:
+      return 1;
+    default:
+      @throw NSInternalInconsistencyException;
+  }
+}
+
+#pragma mark -
+
+- (void)didSelectReveal
+{
+  self.hiddenPIN = NO;
+  [self.tableView reloadData];
 }
 
 @end
