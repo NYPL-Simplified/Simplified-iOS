a0028b993daa38848870526fc0515ba00349bccd
John Nowak <johnnowak@nypl.org>
2015-04-13 13:52:14 -0400

    Simplified approach to NYPLBookCellDelegate

--- Simplified/NYPLBookCellDelegate.h
+++ Simplified/NYPLBookCellDelegate.h
@@ -1,6 +1,11 @@
-#import "NYPLBookCell.h"
+#import "NYPLBookNormalCell.h"
+#import "NYPLBookDownloadFailedCell.h"
+#import "NYPLBookDownloadingCell.h"
 
-@interface NYPLBookCellDelegate : NSObject <NYPLBookCellDelegate>
+/* This class implements a shared delegate that performs all of its duties via the shared registry,
+shared cover registry, shared download center, et cetera. */
+@interface NYPLBookCellDelegate : NSObject
+  <NYPLBookNormalCellDelegate, NYPLBookDownloadFailedCellDelegate, NYPLBookDownloadingCellDelegate>
 
 + (id)new NS_UNAVAILABLE;
 - (id)init NS_UNAVAILABLE;
