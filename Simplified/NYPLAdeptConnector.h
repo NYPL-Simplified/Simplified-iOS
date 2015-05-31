450da42fbb72c25a43ef381516b02f6b6a4e07a3
John Nowak <johnnowak@nypl.org>
2015-05-31 18:36:22 -0400

    Many minor Adept-related improvements

--- Simplified/NYPLAdeptConnector.h
+++ Simplified/NYPLAdeptConnector.h
@@ -24,15 +24,23 @@ didFinishDownloadingToURL:(NSURL *)URL
 
 @property (atomic, weak) id<NYPLAdeptConnectorDelegate> delegate;
 @property (atomic, readonly) BOOL deviceAuthorized;
+@property (atomic, readonly) BOOL workflowsInProgress;
 
 + (NYPLAdeptConnector *)sharedAdeptConnector;
 
+// This can only be called if no workflows are in progress and the device is not authorized. The
+// authorization attempt is handled asynchronously and its success or failure is reported to the
+// delegate.
 - (void)authorizeWithVendorID:(NSString *)vendorID
                      username:(NSString *)username
                      password:(NSString *)password;
 
+// This can only be called if no workflows are in progress. The device is deauthorized when this
+// method returns.
 - (void)deauthorize;
 
+// Fulfillment requests will be ignored if the device is not presently authorized. The fulfillment
+// attempt is handled asynchronously and its status is reported to the delegate.
 - (void)fulfillWithACSMData:(NSData *)ACSMData tag:(NSString *)tag;
 
 @end
