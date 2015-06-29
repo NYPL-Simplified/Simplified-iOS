9b1bae4f7d8ab78c31e7801b0dc90fb0e89a27a4
Winnie Quinn <me@winniequinn.com>
2015-06-28 23:26:08 -0400

    Partial rework of NYPLRemoteViewController with nullability annotations

--- Simplified/NYPLRemoteViewController.h
+++ Simplified/NYPLRemoteViewController.h
@@ -5,21 +5,22 @@
 // function provided will be called. That handler then returns a new view controller that is
 // presented by the instance of |NYPLRemoteViewController|.
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NYPLRemoteViewController : UIViewController
 
-// Neither |URL| nor |handler| may be nil. |completionHandler| will be called if and only if the
-// data was downloaded successfully. In the event it was not, an error will be presented to the user
-// with a button to begin another attempt. Calling this method while a load is already in progress
-// will cancel the previous attempt.
-//
-// NOTE: An instance of |NYPLRemoteViewController| will keep a strong reference to |handler|. If
-// your handler will reference the instance of |NYPLRemoteViewController| to which it is provided,
-// ensure that it references it weakly.
-- (void)loadURL:(NSURL *)URL completionHandler:(UIViewController *(^)(NSData *data))handler;
++ (id)new NS_UNAVAILABLE;
+- (id)init NS_UNAVAILABLE;
+- (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
 
-// This will cancel the current download if one is in progress, then attempt to fetch new data from
-// the URL previously provided. Any currently presented view controller will be dismissed until new
-// data is available, after which the previously provided handler will once again be invoked.
-- (void)reload;
+// |handler| is strongly retained.
+- (instancetype)initWithURL:(NSURL *)URL
+          completionHandler:(UIViewController *(^)(NSData *data))handler;
+
+// This may be called more than once to reload the data accessible at the previously provided URL.
+- (void)load;
 
 @end
+
+NS_ASSUME_NONNULL_END
