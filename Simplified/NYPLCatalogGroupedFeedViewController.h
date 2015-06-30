61e8d4b5ba9a2cb40daa74bcc39f8aae117a6e00
Winnie Quinn <me@winniequinn.com>
2015-06-30 13:06:00 -0400

    Copied old navigation code into new grouped feed view controller

--- Simplified/NYPLCatalogGroupedFeedViewController.h
+++ Simplified/NYPLCatalogGroupedFeedViewController.h
@@ -1,5 +1,11 @@
-#import "NYPLBookCellCollectionViewController.h"
+@interface NYPLCatalogGroupedFeedViewController : UIViewController
 
-@interface NYPLCatalogGroupedFeedViewController : NYPLBookCellCollectionViewController
++ (id)new NS_UNAVAILABLE;
+- (id)init NS_UNAVAILABLE;
+- (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
+
+// designated initializer
+- (instancetype)initWithURL:(NSURL *)URL title:(NSString *)title;
 
 @end
