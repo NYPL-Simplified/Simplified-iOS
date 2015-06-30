11744766bb73bd7eb9fc1a24f1e71a82f58f8203
Winnie Quinn <me@winniequinn.com>
2015-06-30 13:58:08 -0400

    Made NYPLCatalogGroupedFeed compatable with grouped feed data

--- Simplified/NYPLCatalogGroupedFeed.m
+++ Simplified/NYPLCatalogGroupedFeed.m
@@ -3,6 +3,7 @@
 #import "NYPLBookRegistry.h"
 #import "NYPLCatalogLane.h"
 #import "NYPLCatalogSubsectionLink.h"
+#import "NYPLNull.h"
 #import "NYPLOPDS.h"
 #import "NYPLOpenSearchDescription.h"
 #import "NYPLSession.h"
@@ -19,166 +20,71 @@
 
 @implementation NYPLCatalogGroupedFeed
 
-+ (void)withURL:(NSURL *const)URL
-        handler:(void (^)(NYPLCatalogGroupedFeed *root))handler
+- (instancetype)initWithOPDSFeed:(NYPLOPDSFeed *)feed
 {
-  if(!handler) {
+  if(feed.type != NYPLOPDSFeedTypeAcquisitionGrouped) {
     @throw NSInvalidArgumentException;
   }
   
-  [NYPLOPDSFeed
-   withURL:URL
-   completionHandler:^(NYPLOPDSFeed *const navigationFeed) {
-     if(!navigationFeed) {
-       NYPLLOG(@"Failed to retrieve main navigation feed.");
-       NYPLAsyncDispatch(^{handler(nil);});
-       return;
-     }
-     
-     NSURL *openSearchURL = nil;
-     
-     for(NYPLOPDSLink *const link in navigationFeed.links) {
-       if([link.rel isEqualToString:NYPLOPDSRelationSearch] &&
-          NYPLOPDSTypeStringIsOpenSearchDescription(link.type)) {
-         openSearchURL = link.href;
-       }
-     }
-     
-     NSMutableSet *const featuredURLs =
-     [NSMutableSet setWithCapacity:navigationFeed.entries.count];
-     
-     for(NYPLOPDSEntry *const entry in navigationFeed.entries) {
-       for(NYPLOPDSLink *const link in entry.links) {
-         if([link.rel isEqualToString:NYPLOPDSRelationFeatured]) {
-           [featuredURLs addObject:link.href];
-         }
-       }
-     }
-     
-     [[NYPLSession sharedSession]
-      withURLs:featuredURLs handler:^(NSDictionary *const URLsToDataOrNull) {
-        NSMutableArray *const lanes =
-        [NSMutableArray arrayWithCapacity:navigationFeed.entries.count];
-        
-        for(NYPLOPDSEntry *const navigationEntry in navigationFeed.entries) {
-          NSURL *featuredURL = nil;
-          NYPLCatalogSubsectionLink *subsectionLink = nil;
-          
-          for(NYPLOPDSLink *const link in navigationEntry.links) {
-            if([link.rel isEqualToString:NYPLOPDSRelationFeatured]) {
-              if(!NYPLOPDSTypeStringIsAcquisition(link.type)) {
-                NYPLLOG(@"Ignoring featured feed without acquisition type.");
-              } else {
-                featuredURL = link.href;
-              }
-            } else {
-              // TODO: We assume the last acquisition or navigation feed is the main feed for the
-              // lane. Is there a relation we should be using to do a better job of this? We
-              // previously used 'subsection', but it's unclear to me if that is appropriate for
-              // acquisition feeds.
-              if(NYPLOPDSTypeStringIsAcquisition(link.type)) {
-                subsectionLink = [[NYPLCatalogSubsectionLink alloc]
-                                  initWithType:NYPLCatalogSubsectionLinkTypeAcquisition
-                                  URL:link.href];
-              } else if(NYPLOPDSTypeStringIsNavigation(link.type)) {
-                subsectionLink = [[NYPLCatalogSubsectionLink alloc]
-                                  initWithType:NYPLCatalogSubsectionLinkTypeNavigation
-                                  URL:link.href];
-              }
-            }
-          }
-          
-          if(!subsectionLink) {
-            NYPLLOG(@"Discarding entry without subsection.");
-            continue;
-          }
-          
-          if(!featuredURL) {
-            NYPLLOG(@"Creating lane without featured books.");
-            [lanes addObject:
-             [[NYPLCatalogLane alloc]
-              initWithBooks:@[]
-              subsectionLink:subsectionLink
-              title:navigationEntry.title]];
-            continue;
-          }
-          
-          id const featuredDataObject = URLsToDataOrNull[featuredURL];
-          if([featuredDataObject isKindOfClass:[NSNull class]]) {
-            NYPLLOG(@"Creating lane without unobtainable featured books.");
-            [lanes addObject:
-             [[NYPLCatalogLane alloc]
-              initWithBooks:@[]
-              subsectionLink:subsectionLink
-              title:navigationEntry.title]];
-            continue;
-          }
-          
-          NSData *const featuredData = featuredDataObject;
-          assert([featuredData isKindOfClass:[NSData class]]);
-          
-          NYPLXML *const feedXML = [NYPLXML XMLWithData:featuredData];
-          
-          if(!feedXML) {
-            NYPLLOG(@"Creating lane without unparsable featured books.");
-            [lanes addObject:
-             [[NYPLCatalogLane alloc]
-              initWithBooks:@[]
-              subsectionLink:subsectionLink
-              title:navigationEntry.title]];
-            continue;
-          }
-          
-          NYPLOPDSFeed *featuredAcquisitionFeed = [[NYPLOPDSFeed alloc] initWithXML:feedXML];
-          
-          if(!featuredAcquisitionFeed) {
-            NYPLLOG(@"Creating lane without invalid featured books.");
-            [lanes addObject:
-             [[NYPLCatalogLane alloc]
-              initWithBooks:@[]
-              subsectionLink:subsectionLink
-              title:navigationEntry.title]];
-            continue;
-          }
-          
-          NSMutableArray *const books =
-          [NSMutableArray arrayWithCapacity:featuredAcquisitionFeed.entries.count];
-          
-          for(NYPLOPDSEntry *const acquisitionEntry in featuredAcquisitionFeed.entries) {
-            NYPLBook *const book = [NYPLBook bookWithEntry:acquisitionEntry];
-            if(!book) {
-              NYPLLOG(@"Failed to create book from entry.");
-              continue;
-            }
-            [[NYPLBookRegistry sharedRegistry] updateBook:book];
-            [books addObject:book];
-          }
-          
-          [lanes addObject:
-           [[NYPLCatalogLane alloc]
-            initWithBooks:books
-            subsectionLink:subsectionLink
-            title:navigationEntry.title]];
-        }
-        
-        if(openSearchURL) {
-          [NYPLOpenSearchDescription
-           withURL:openSearchURL
-           completionHandler:^(NYPLOpenSearchDescription *const description) {
-             if(!description) {
-               NYPLLOG(@"Failed to retrieve OpenSearch description document.");
-             }
-             NYPLAsyncDispatch(^{handler([[NYPLCatalogGroupedFeed alloc]
-                                          initWithLanes:lanes
-                                          searchTemplate:description.OPDSURLTemplate]);});
-           }];
-        } else {
-          NYPLAsyncDispatch(^{handler([[NYPLCatalogGroupedFeed alloc]
-                                       initWithLanes:lanes
-                                       searchTemplate:nil]);});
-        }
-      }];
-   }];
+  NSURL *openSearchURL = nil;
+  
+  for(NYPLOPDSLink *const link in feed.links) {
+    if([link.rel isEqualToString:NYPLOPDSRelationSearch] &&
+       NYPLOPDSTypeStringIsOpenSearchDescription(link.type)) {
+      openSearchURL = link.href;
+      break;
+    }
+  }
+  
+  
+  // This holds group titles in order, without duplicates.
+  NSMutableArray *const groupTitles = [NSMutableArray array];
+  
+  NSMutableDictionary *const groupTitleToMutableBookArray = [NSMutableDictionary dictionary];
+  NSMutableDictionary *const groupTitleToURLOrNull = [NSMutableDictionary dictionary];
+  
+  for(NYPLOPDSEntry *const entry in feed.entries) {
+    if(!entry.groupAttributes) {
+      NYPLLOG(@"Ignoring entry with missing group.");
+      continue;
+    }
+    
+    NSString *const groupTitle = entry.groupAttributes.title;
+    
+    NYPLBook *const book = [NYPLBook bookWithEntry:entry];
+    if(!book) {
+      NYPLLOG(@"Failed to create book from entry.");
+      continue;
+    }
+    
+    // We sync the latest metadata with the registry just in case it has changed.
+    [[NYPLBookRegistry sharedRegistry] updateBook:book];
+    
+    NSMutableArray *const bookArray = groupTitleToMutableBookArray[groupTitle];
+    if(bookArray) {
+      // We previously found a book in this group, so we can just add one more.
+      [bookArray addObject:book];
+    } else {
+      // This is the first book we've found in this group, so we need to do a few things.
+      [groupTitles addObject:groupTitle];
+      groupTitleToMutableBookArray[groupTitle] = [NSMutableArray arrayWithObject:book];
+      groupTitleToURLOrNull[groupTitle] = NYPLNullFromNil(entry.groupAttributes.href);
+    }
+  }
+  
+  NSMutableArray *const lanes = [NSMutableArray array];
+  
+  for(NSString *const groupTitle in groupTitles) {
+    [lanes addObject:[[NYPLCatalogLane alloc]
+                      initWithBooks:groupTitleToMutableBookArray[groupTitle]
+                      subsectionLink:NYPLNullToNil(groupTitleToURLOrNull[groupTitle])
+                      title:groupTitle]];
+  }
+  
+  // FIXME: |searchTemplate:| is passed nil because this method needs to return immediately and
+  // getting the template requires network access. What we should do instead is just store the
+  // URL of the open search document and let the view controller fetch it later.
+  return [self initWithLanes:lanes searchTemplate:nil];
 }
 
 - (instancetype)initWithLanes:(NSArray *const)lanes
