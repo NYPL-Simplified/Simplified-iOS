4888fdf01d89625b4ac6ea27fb95ce3f6db756f4
Winnie Quinn <me@winniequinn.com>
2015-06-30 13:09:39 -0400

    Copied old navigation feed code into NYLCatalogGroupedFeed

--- Simplified/NYPLCatalogGroupedFeed.h
+++ Simplified/NYPLCatalogGroupedFeed.h
@@ -1,3 +1,16 @@
 @interface NYPLCatalogGroupedFeed : NSObject
 
+@property (nonatomic, readonly) NSArray *lanes;
+@property (nonatomic, readonly) NSString *searchTemplate; // nilable
+
++ (id)new NS_UNAVAILABLE;
+- (id)init NS_UNAVAILABLE;
+
+// In the callback, |navigationFeed| will be |nil| if an error occurred.
++ (void)withURL:(NSURL *)URL handler:(void (^)(NYPLCatalogGroupedFeed *groupedFeed))handler;
+
+// designated initializer
+- (instancetype)initWithLanes:(NSArray *)lanes
+               searchTemplate:(NSString *)searchTemplate;
+
 @end
