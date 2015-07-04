0c348dc9abc8bedce66bc71e4c0681345a3d87d9
Winnie Quinn <me@winniequinn.com>
2015-07-03 22:22:46 -0400

    Removed old initialization code in NYPLCatalogUngroupedFeed

--- Simplified/NYPLCatalogUngroupedFeed.m
+++ Simplified/NYPLCatalogUngroupedFeed.m
@@ -28,7 +28,6 @@ static NSUInteger const preloadThreshold = 100;
 @implementation NYPLCatalogUngroupedFeed
 
 + (void)withURL:(NSURL *)URL
-includingSearchTemplate:(BOOL)includingSearchTemplate
 handler:(void (^)(NYPLCatalogUngroupedFeed *category))handler
 {
   if(!handler) {
@@ -38,103 +37,13 @@ handler:(void (^)(NYPLCatalogUngroupedFeed *category))handler
   [NYPLOPDSFeed
    withURL:URL
    completionHandler:^(NYPLOPDSFeed *const ungroupedFeed) {
-     if(!ungroupedFeed) {
-       NYPLLOG(@"Failed to retrieve acquisition feed.");
-       NYPLAsyncDispatch(^{handler(nil);});
-       return;
+     if(ungroupedFeed.type != NYPLOPDSFeedTypeAcquisitionUngrouped) {
+       NYPLLOG(@"Ignoring feed of invalid type.");
+       handler(nil);
      }
      
-     NSMutableArray *const books = [NSMutableArray arrayWithCapacity:ungroupedFeed.entries.count];
-     
-     for(NYPLOPDSEntry *const entry in ungroupedFeed.entries) {
-       NYPLBook *const book = [NYPLBook bookWithEntry:entry];
-       if(!book) {
-         NYPLLOG(@"Failed to create book from entry.");
-         continue;
-       }
-       [[NYPLBookRegistry sharedRegistry] updateBook:book];
-       [books addObject:book];
-     }
-     
-     NSMutableArray *const facetGroupNames = [NSMutableArray array];
-     NSMutableDictionary *const facetGroupNamesToMutableFacetArrays =
-       [NSMutableDictionary dictionary];
-     NSURL *nextURL = nil;
-     NSURL *openSearchURL = nil;
-     
-     for(NYPLOPDSLink *const link in ungroupedFeed.links) {
-       if([link.rel isEqualToString:NYPLOPDSRelationFacet]) {
-         NSString *groupName = nil;
-         for(NSString *const key in link.attributes) {
-           if(NYPLOPDSAttributeKeyStringIsFacetGroup(key)) {
-             groupName = link.attributes[key];
-             break;
-           }
-         }
-         if(!groupName) {
-           NYPLLOG(@"Ignoring facet without group due to UI limitations.");
-           continue;
-         }
-         NYPLCatalogFacet *const facet = [NYPLCatalogFacet catalogFacetWithLink:link];
-         if(!facet) {
-           NYPLLOG(@"Ignoring invalid facet link.");
-           continue;
-         }
-         if(![facetGroupNames containsObject:groupName]) {
-           [facetGroupNames addObject:groupName];
-           facetGroupNamesToMutableFacetArrays[groupName] = [NSMutableArray arrayWithCapacity:2];
-         }
-         [facetGroupNamesToMutableFacetArrays[groupName] addObject:facet];
-         continue;
-       }
-       if([link.rel isEqualToString:NYPLOPDSRelationPaginationNext]) {
-         nextURL = link.href;
-         continue;
-       }
-       if([link.rel isEqualToString:NYPLOPDSRelationSearch] &&
-          NYPLOPDSTypeStringIsOpenSearchDescription(link.type)) {
-         openSearchURL = link.href;
-         continue;
-       }
-     }
-     
-     // Care is taken to preserve facet and facet group order from the original feed.
-     NSMutableArray *const facetGroups = [NSMutableArray arrayWithCapacity:facetGroupNames.count];
-     for(NSString *const facetGroupName in facetGroupNames) {
-       [facetGroups addObject:[[NYPLCatalogFacetGroup alloc]
-                               initWithFacets:facetGroupNamesToMutableFacetArrays[facetGroupName]
-                               name:facetGroupName]];
-     }
-     
-     if(openSearchURL && includingSearchTemplate) {
-       [NYPLOpenSearchDescription
-        withURL:openSearchURL
-        completionHandler:^(NYPLOpenSearchDescription *const description) {
-          if(!description) {
-            NYPLLOG(@"Failed to retrieve open search description.");
-          }
-          NYPLAsyncDispatch(^{handler([[NYPLCatalogUngroupedFeed alloc]
-                                       initWithBooks:books
-                                       facetGroups:facetGroups
-                                       nextURL:nextURL
-                                       searchTemplate:description.OPDSURLTemplate
-                                       title:ungroupedFeed.title]);});
-        }];
-      } else {
-        NYPLAsyncDispatch(^{handler([[NYPLCatalogUngroupedFeed alloc]
-                                     initWithBooks:books
-                                     facetGroups:facetGroups
-                                     nextURL:nextURL
-                                     searchTemplate:nil
-                                     title:ungroupedFeed.title]);});
-      }
+     handler([[self alloc] initWithOPDSFeed:ungroupedFeed]);
    }];
-}
-
-+ (void)withURL:(NSURL *)URL
-        handler:(void (^)(NYPLCatalogUngroupedFeed *category))handler
-{
-  [self withURL:URL includingSearchTemplate:YES handler:handler];
 }
 
 - (instancetype)initWithOPDSFeed:(NYPLOPDSFeed *const)feed
@@ -273,7 +182,6 @@ handler:(void (^)(NYPLCatalogUngroupedFeed *category))handler
   
   [NYPLCatalogUngroupedFeed
    withURL:self.nextURL
-   includingSearchTemplate:NO
    handler:^(NYPLCatalogUngroupedFeed *const ungroupedFeed) {
      [[NSOperationQueue mainQueue] addOperationWithBlock:^{
        if(!ungroupedFeed) {
