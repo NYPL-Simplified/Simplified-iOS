cd5dcc7feb715ad73fe53757955c63f8dd8c8039
Raduz Benicky <raduzb@evidentpoint.com>
2015-08-10 12:32:32 -0700

    Issue #191 Changed Credits & Acknowledgements to Acknowledgements as requested

--- Simplified/NYPLSettingsPrimaryTableViewController.m
+++ Simplified/NYPLSettingsPrimaryTableViewController.m
@@ -99,7 +99,7 @@ didSelectRowAtIndexPath:(NSIndexPath *const)indexPath
       UITableViewCell *const cell = [[UITableViewCell alloc]
                                      initWithStyle:UITableViewCellStyleDefault
                                      reuseIdentifier:nil];
-      cell.textLabel.text = NSLocalizedString(@"CreditsAndAcknowledgements", nil);
+      cell.textLabel.text = NSLocalizedString(@"Acknowledgements", nil);
       cell.textLabel.font = [UIFont systemFontOfSize:17];
       if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
