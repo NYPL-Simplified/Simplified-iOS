efc1648babe345202bbbb1589f644309ad8c1417
Ben Anderman <bena@evidentpoint.com>
2015-08-20 15:06:56 -0700

    [issue #184] Implement holds functionality
    Add clock icons to return now/cancel hold buttons.

--- Simplified/NYPLRoundedButton.h
+++ Simplified/NYPLRoundedButton.h
@@ -1,3 +1,9 @@
+typedef NS_ENUM(NSInteger, NYPLRoundedButtonType) {
+  NYPLRoundedButtonTypeNormal = 0,
+  NYPLRoundedButtonTypeClock,
+  NYPLRoundedButtonTypeQueue
+};
+
 @interface NYPLRoundedButton : UIButton
 
 + (id)buttonWithType:(UIButtonType)buttonType NS_UNAVAILABLE;
@@ -5,6 +11,10 @@
 - (id)init NS_UNAVAILABLE;
 - (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (id)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+
+@property (nonatomic) NYPLRoundedButtonType type;
+@property (nonatomic) NSInteger queuePosition;
+@property (nonatomic) NSDate *endDate;
 
 + (instancetype)button;
 
--- Simplified/NYPLRoundedButton.m
+++ Simplified/NYPLRoundedButton.m
@@ -1,4 +1,12 @@
 #import "NYPLRoundedButton.h"
+#import "NSDate+NYPLDateAdditions.h"
+
+@interface NYPLRoundedButton ()
+
+@property (nonatomic) UIImageView *iconView;
+@property (nonatomic) UILabel *label;
+
+@end
 
 @implementation NYPLRoundedButton
 
@@ -11,8 +19,63 @@
   button.layer.borderColor = button.tintColor.CGColor;
   button.layer.borderWidth = 1;
   button.layer.cornerRadius = 3;
+  
+  button.iconView = [UIImageView new];
+  button.label = [UILabel new];
+  button.label.textColor = button.tintColor;
+  button.label.font = [UIFont systemFontOfSize:9];
+  
+  [button addSubview:button.iconView];
+  [button addSubview:button.label];
+  
+  button.type = NYPLRoundedButtonTypeNormal;
 
   return button;
+}
+
+- (void)setType:(NYPLRoundedButtonType)type
+{
+  _type = type;
+  [self updateViews];
+}
+
+- (void)setQueuePosition:(NSInteger)queuePosition
+{
+  _queuePosition = queuePosition;
+  [self updateViews];
+}
+
+- (void)setEndDate:(NSDate *)endDate
+{
+  _endDate = endDate;
+  [self updateViews];
+}
+
+- (void)updateViews
+{
+  if (self.type == NYPLRoundedButtonTypeNormal) {
+    self.contentEdgeInsets = UIEdgeInsetsZero;
+    self.iconView.hidden = YES;
+    self.label.hidden = YES;
+  } else {
+    NSString *imageName = self.type == NYPLRoundedButtonTypeClock ? @"Clock" : @"Clock"; // TODO: get queue icon
+    self.iconView.image = [[UIImage imageNamed:imageName] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    self.iconView.hidden = NO;
+    self.label.hidden = NO;
+    if (self.type == NYPLRoundedButtonTypeClock) {
+      self.label.text = [self.endDate shortTimeUntilString];
+    } else {
+      self.label.text = [@(self.queuePosition) stringValue];
+    }
+    
+    [self.label sizeToFit];
+    
+    self.iconView.frame = CGRectMake(8, 3, 14, 14);
+    CGRect frame = self.label.frame;
+    frame.origin = CGPointMake(self.iconView.center.x - frame.size.width/2, CGRectGetMaxY(self.iconView.frame));
+    self.label.frame = frame;
+    self.contentEdgeInsets = UIEdgeInsetsMake(6, self.iconView.frame.size.width + 8, 6, 0);
+  }
 }
 
 #pragma mark UIView
@@ -29,6 +92,7 @@
   [super tintColorDidChange];
   
   self.layer.borderColor = self.tintColor.CGColor;
+  self.label.textColor = self.tintColor;
 }
 
 @end
