a6c1efd78219afdd860a3ba1a5c42262353cd40f
Ben Anderman <bena@evidentpoint.com>
2015-08-26 14:38:32 -0700

    Add badge for Holds view, indicating the number of ready to checkout books there are.

--- Simplified/NYPLHoldsViewController.h
+++ Simplified/NYPLHoldsViewController.h
@@ -5,6 +5,8 @@
 - (id)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
 
+- (void)updateBadge;
+
 // designated initializer
 - (instancetype)init;
 
