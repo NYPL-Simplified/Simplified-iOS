d0336b685e10716e64f9fbb179598a4f5e3d34aa
Raduz Benicky <raduzb@evidentpoint.com>
2015-08-26 10:35:41 -0700

    Execute book length calculation on a background thread Do not update book progression if book calculation has not finished yet.

--- Simplified/NYPLReaderReadiumView.m
+++ Simplified/NYPLReaderReadiumView.m
@@ -321,7 +321,10 @@ navigationType:(__attribute__((unused)) UIWebViewNavigationType)navigationType
   }
   
   self.package.rootURL = [NSString stringWithFormat:@"http://127.0.0.1:%d/", self.server.port];
-  [self calculateBookLength];
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),^{
+    [self calculateBookLength];
+  });
   
   NSMutableDictionary *const dictionary = [NSMutableDictionary dictionary];
   dictionary[@"package"] = self.package.dictionary;
@@ -459,6 +462,8 @@ navigationType:(__attribute__((unused)) UIWebViewNavigationType)navigationType
 }
 
 -(void) calculateProgressionWithDictionary:(NSDictionary *const)dictionary withHandler:(void(^)(void))handler {
+  if (!self.bookMapDictionary) return;
+  
   NSArray *openPagesArray = [dictionary objectForKey:@"openPages"];
   NSDictionary *openPagesDict = [openPagesArray firstObject];
   
