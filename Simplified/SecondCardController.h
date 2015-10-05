ebbe7c227d01859dfd784f6c8974faaab9f0ec27
Sam Tarakajian <samtarakajian@nypl.org>
2015-10-05 09:36:29 -0400

    Just starting to play around with using a storyboard to manage the flow of cards through the registration process

---
+++ Simplified/SecondCardController.h
@@ -0,0 +1,13 @@
+//
+//  SecondCardController.h
+//  Simplified
+//
+//  Created by Sam Tarakajian on 10/5/15.
+//  Copyright © 2015 NYPL Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondCardController : UIViewController
+
+@end
---
+++ Simplified/ThirdCardController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdCardController.h
+//  Simplified
+//
+//  Created by Sam Tarakajian on 10/5/15.
+//  Copyright © 2015 NYPL Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdCardController : UIViewController
+
+@end
