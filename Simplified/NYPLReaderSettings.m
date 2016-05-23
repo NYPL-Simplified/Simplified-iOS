e4be2279eecb381df54aadd245c0b20ab5279638
Winnie Quinn <me@winniequinn.com>
2016-05-23 18:55:44 -0400

    Adjusted font size scaling and padding

--- Simplified/NYPLReaderSettings.m
+++ Simplified/NYPLReaderSettings.m
@@ -412,7 +412,7 @@ static NSString *const MediaOverlaysEnableClick = @"mediaOverlaysEnableClick";
 
 - (NSDictionary *)readiumSettingsRepresentation
 {
-  CGFloat const scalingFactor = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 1.5 : 4.0;
+  CGFloat const scalingFactor = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 1.1 : 1.5;
   
   CGFloat baseSize;
   switch(self.fontSize) {
