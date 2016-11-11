dac0d3123d492ffb56be22c16bc76291238fe29f
Aferdita Muriqi <aferditamuriqi@nypl.org>
2016-11-11 18:40:23 -0500

    sync account if authentication is needed, if not needed (for example for the instant classics) just reload the registry

--- Simplified/NYPLMyBooksViewController.m
+++ Simplified/NYPLMyBooksViewController.m
@@ -17,6 +17,8 @@
 
 #import "NYPLMyBooksViewController.h"
 
+#import "SimplyE-Swift.h"
+
 // order-dependent
 typedef NS_ENUM(NSInteger, Group) {
   GroupSortBy,
@@ -348,18 +350,27 @@ OK:
 
 - (void)didSelectSync
 {
-  if([[NYPLAccount sharedAccount] hasBarcodeAndPIN]) {
-    [[NYPLBookRegistry sharedRegistry] syncWithStandardAlertsOnCompletion];
-  } else {
-    // We can't sync if we're not logged in, so let's log in. We don't need a completion handler
-    // here because logging in will trigger a sync anyway. The only downside of letting the sync
-    // happen elsewhere is that the user will not receive an error if the sync fails because it will
-    // be considered an automatic sync and not a manual sync.
-    // TODO: We should make this into a manual sync while somehow avoiding double-syncing.
-    [NYPLSettingsAccountSignInViewController
-     requestCredentialsUsingExistingBarcode:NO
-     completionHandler:nil];
-    [self.refreshControl endRefreshing];
+  Account *account = [[NYPLSettings sharedSettings] currentAccount];
+  
+  if (account.needsAuth)
+  {
+    if([[NYPLAccount sharedAccount] hasBarcodeAndPIN]) {
+      [[NYPLBookRegistry sharedRegistry] syncWithStandardAlertsOnCompletion];
+    } else {
+      // We can't sync if we're not logged in, so let's log in. We don't need a completion handler
+      // here because logging in will trigger a sync anyway. The only downside of letting the sync
+      // happen elsewhere is that the user will not receive an error if the sync fails because it will
+      // be considered an automatic sync and not a manual sync.
+      // TODO: We should make this into a manual sync while somehow avoiding double-syncing.
+      [NYPLSettingsAccountSignInViewController
+       requestCredentialsUsingExistingBarcode:NO
+       completionHandler:nil];
+      [self.refreshControl endRefreshing];
+    }
+  }
+  else
+  {
+    [[NYPLBookRegistry sharedRegistry] justLoad];
   }
 }
 
