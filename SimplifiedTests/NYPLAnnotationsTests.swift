1d9bfa4ce6c206f17805f702bd31885fce7d1922
Jerry Horton <jhorton@Jerrys-Mac-Pro.local>
2016-11-22 01:21:45 -0600

    - added test for encode/decode of LastReadBookOperationArray to simulate going from forground/background and terminate/foreground

--- SimplifiedTests/NYPLAnnotationsTests.swift
+++ SimplifiedTests/NYPLAnnotationsTests.swift
@@ -7,12 +7,36 @@
 //
 
 import XCTest
+@testable import SimplyE
 
 class NYPLAnnotationsTests: XCTestCase {
     
+    var book: NYPLBook!
+    var entry: NYPLOPDSEntry!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let pathString = Bundle(for: type(of: self)).path(forResource: "single_entry", ofType: "xml")
+        XCTAssertNotNil(pathString)
+        let data = NSData( contentsOfFile: pathString!)
+        XCTAssertNotNil(data)
+        
+        let feedXML: NYPLXML = NYPLXML(data: data as Data!)
+        XCTAssertNotNil(feedXML)
+        
+        let feed: NYPLOPDSFeed = NYPLOPDSFeed.init(xml: feedXML)
+        XCTAssertNotNil(feed)
+        
+        let entries = feed.entries as! [NYPLOPDSEntry]
+        XCTAssertNotNil(entries)
+        
+        self.entry = entries[0]
+        XCTAssertNotNil(self.entry)
+        
+        self.book = NYPLBook.init(entry: self.entry)
+        XCTAssertNotNil(self.book)
+        
     }
     
     override func tearDown() {
@@ -20,16 +44,24 @@ class NYPLAnnotationsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testLastReadBookOperationArrayEncodeDecode() {
+        
+        let lastReadOperation1 = NYPLLastReadBookOperation(cfi: "page 1", book: self.book)
+        XCTAssertNotNil(lastReadOperation1)
+        
+        let lastReadOperation2 = NYPLLastReadBookOperation(cfi: "page 2", book: self.book)
+        XCTAssertNotNil(lastReadOperation2)
+        
+        let operationEncodeArray = [lastReadOperation1,lastReadOperation2]
+        
+        let file: String = "lastReadOperations.plist"
+        let dir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true).first
+        XCTAssertNotNil(dir)
+        let path = URL(fileURLWithPath: dir!).appendingPathComponent(file)
+        XCTAssertNotNil(path)
+        XCTAssertTrue(NSKeyedArchiver.archiveRootObject(operationEncodeArray, toFile: (path.path)))
+        let operationDecodeArray = NSKeyedUnarchiver.unarchiveObject(withFile: path.path) as! [NYPLLastReadBookOperation]
+        XCTAssertNotNil(operationDecodeArray)
     }
     
 }
--- SimplifiedTests/NYPLCirculationAnalyticsTests.swift
+++ SimplifiedTests/NYPLCirculationAnalyticsTests.swift
@@ -64,11 +64,5 @@ class NYPLCirculationAnalyticsTests: XCTestCase {
         XCTAssertNotNil(operationDecodeArray)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
