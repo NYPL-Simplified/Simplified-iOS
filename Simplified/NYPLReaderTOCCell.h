dfc860f2f5b6f5148e459fb5dcc9d895cd09f7f3
Greg O'Neill <gioneill@gmail.com>
2017-04-21 13:08:52 -0400

    Enabled dynamic row heights. Changed TOC element indenting to auto layout. Updated IB for font/style

--- Simplified/NYPLReaderTOCCell.h
+++ Simplified/NYPLReaderTOCCell.h
@@ -1,10 +1,6 @@
-// TODO: This class pulls the current color scheme from NYPLReaderSettings. This may or may not be
-// an appropriate way of handling color schemes. If the color scheme changes, all tables containing
-// a NYPLReaderTOCCell must be reloaded.
-
 @interface NYPLReaderTOCCell : UITableViewCell
 
-@property (nonatomic) NSUInteger nestingLevel;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leadingEdgeConstraint;
 
 @end
