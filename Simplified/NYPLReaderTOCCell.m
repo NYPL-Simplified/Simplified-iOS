b97a1066ddfc965a4ab2d70f0f6061dcc71b75f8
Aferdita Muriqi <aferditamuriqi@nypl.org>
2017-04-21 09:19:28 -0400

    fixed formatting

--- Simplified/NYPLReaderTOCCell.m
+++ Simplified/NYPLReaderTOCCell.m
@@ -10,9 +10,9 @@
   
   CGRect frame = self.contentView.bounds;
   if (self.nestingLevel > 0) {
-  frame.origin.x = self.nestingLevel * 20 + 10;
-  frame.size.width -= self.nestingLevel * 20 + 20;
-  self.titleLabel.frame = frame;
+    frame.origin.x = self.nestingLevel * 20 + 10;
+    frame.size.width -= self.nestingLevel * 20 + 20;
+    self.titleLabel.frame = frame;
   }
 }
 
