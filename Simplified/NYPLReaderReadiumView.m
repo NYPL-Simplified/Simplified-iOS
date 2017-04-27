963b4f900907cdc54ee59a941c2563528b1014fa
Aferdita Muriqi <aferditamuriqi@nypl.org>
2017-04-27 16:30:50 -0400

    removed comments at the end of a line of code, if this comments are needed, place them above.

--- Simplified/NYPLReaderReadiumView.m
+++ Simplified/NYPLReaderReadiumView.m
@@ -687,9 +687,9 @@ decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
         {
           NYPLLOG(error);
         }
-      }]; // end sequentiallyEvaluateJavaScript
+      }];
       
-    } // end if bookmark.idref == idref
+    }
     
     // a bookmark was not found
     completionHandler(NO, nil);
