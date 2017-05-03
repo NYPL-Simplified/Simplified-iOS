dc992c297b7c78848e8849b18dee759a4a6dfb9d
Aferdita Muriqi <aferditamuriqi@nypl.org>
2017-05-03 01:16:06 -0400

    updated bookmark element and added isEqual method

--- Simplified/NYPLReaderBookmarkElement.swift
+++ Simplified/NYPLReaderBookmarkElement.swift
@@ -15,16 +15,19 @@ import UIKit
   var contentCFI:String
   var idref:String
   
-  var chapter:String
-  var page:String
+  var chapter:String?
+  var page:String?
   
-  init(annotationId:String, contentCFI:String, idref:String, chapter:String, page:String)
+  var location:String?
+  
+  init(annotationId:String, contentCFI:String, idref:String, chapter:String?, page:String?, location:String?)
   {
     self.annotationId = annotationId
     self.contentCFI = contentCFI
     self.idref = idref
-    self.chapter = chapter
-    self.page = page
+    self.chapter = chapter ?? ""
+    self.page = page ?? ""
+    self.location = location ?? ""
   }
   
   init(dictionary:NSDictionary)
@@ -32,8 +35,9 @@ import UIKit
     self.annotationId = dictionary["annotationId"] as! String
     self.contentCFI = dictionary["contentCFI"] as! String
     self.idref = dictionary["idref"] as! String
-    self.chapter = dictionary["chapter"] as! String
-    self.page = dictionary["page"] as! String
+    self.chapter = dictionary["chapter"] as? String
+    self.page = dictionary["page"] as? String
+    self.location = dictionary["location"] as? String
   }
 
   var dictionaryRepresentation:NSDictionary {
@@ -41,9 +45,24 @@ import UIKit
     return ["annotationId":self.annotationId,
             "contentCFI":self.contentCFI,
             "idref":self.idref,
-            "chapter":self.chapter,
-            "page":self.page]
+            "chapter":self.chapter ?? "",
+            "page":self.page ?? "",
+            "location":self.location ?? ""]
   }
   
+  override func isEqual(_ object: Any?) -> Bool {
+    
+    let other = object as! NYPLReaderBookmarkElement
+    
+    if (self.annotationId == other.annotationId &&
+      self.contentCFI == other.contentCFI &&
+      self.idref == other.idref &&
+      self.chapter == other.chapter &&
+      self.location == other.location)
+    {
+      return true
+    }
+    return false
+  }
   
 }
