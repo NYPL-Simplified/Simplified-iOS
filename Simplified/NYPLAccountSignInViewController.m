6a2671d85affa39e7803948a478dfb2fb91292bf
Vui Nguyen <vui_engineer@yahoo.com>
2018-08-07 13:39:54 -0600

    use more concise unused notation

--- Simplified/NYPLAccountSignInViewController.m
+++ Simplified/NYPLAccountSignInViewController.m
@@ -831,7 +831,7 @@ completionHandler:(void (^)(void))handler
   NSURLSessionDataTask *const task =
     [self.session
      dataTaskWithRequest:request
-     completionHandler:^(NSData *data __attribute__((unused)),
+     completionHandler:^(__unused NSData *data,
                          NSURLResponse *const response,
                          NSError *const error) {
        
