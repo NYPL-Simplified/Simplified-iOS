991d80ae654b2c3eceb2370ba21e8e374d6233e9
Greg O'Neill <gioneill@gmail.com>
2018-10-23 12:46:14 -0400

    Update grouped and ungrouped feed view controllers with new facet bar

--- Simplified/NYPLCatalogUngroupedFeedViewController.h
+++ Simplified/NYPLCatalogUngroupedFeedViewController.h
@@ -2,16 +2,11 @@
 @class NYPLRemoteViewController;
 @class NYPLOpenSearchDescription;
 @class NYPLCatalogFacet;
+@class NYPLFacetBarView;
 
 #import "NYPLBookCellCollectionViewController.h"
 
 @interface NYPLCatalogUngroupedFeedViewController : NYPLBookCellCollectionViewController
-
-@property (nonatomic) NYPLOpenSearchDescription *searchDescription;
-@property (nonatomic) NYPLCatalogUngroupedFeed *feed;
-@property (nonatomic, readonly) UIRefreshControl *collectionViewRefreshControl;
-@property (nonatomic) UIVisualEffectView *entryPointBarView;
-
 
 
 + (id)new NS_UNAVAILABLE;
@@ -22,8 +17,5 @@
 // |remoteViewController| is weakly referenced.
 - (instancetype)initWithUngroupedFeed:(NYPLCatalogUngroupedFeed *)feed
                  remoteViewController:(NYPLRemoteViewController *)remoteViewController;
-
-- (void)configureEntryPointFacets:(NSArray<NYPLCatalogFacet *> *)facets;
-
 
 @end
