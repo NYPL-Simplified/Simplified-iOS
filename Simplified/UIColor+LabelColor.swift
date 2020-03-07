d70bd984e0e9d6e59c3db38b8e8e312b66ca59dc
Ernest Fan <ernest@Ernests-MacBook-Pro.local>
2020-03-06 16:46:02 -0800

    Create an uicolor extension for label color and refactor code

---
+++ Simplified/UIColor+LabelColor.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@objc extension UIColor {
+  @objc class public func defaultLabelColor() -> UIColor {
+    if #available(iOS 13, *) {
+      return UIColor.label;
+    } else {
+      return UIColor.black;
+    }
+  }
+}
