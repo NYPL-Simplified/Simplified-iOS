91db932667c12bf19ccdb713329f7c8d38f4b571
Ernest Fan <ernest@Ernests-MacBook-Pro.local>
2020-03-06 16:59:49 -0800

    Remove unnecessary keyword

--- Simplified/Utilities/UIColor+LabelColor.swift
+++ Simplified/Utilities/UIColor+LabelColor.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-@objc extension UIColor {
+extension UIColor {
   @objc class public func defaultLabelColor() -> UIColor {
     if #available(iOS 13, *) {
       return UIColor.label;
