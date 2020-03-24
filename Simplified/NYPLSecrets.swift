8f8593e706c41cc7058d1ebcdf31ee44af638e05
Ernest Fan <ernest@Ernests-MacBook-Pro-2.local>
2020-03-23 18:39:36 -0700

    Add NYPLSecrets and update README

---
+++ Simplified/NYPLSecrets.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+enum NYPLSecrets {
+  private static let salt: [UInt8] = [0]
+
+  static var feedbooks:String {
+    let encoded: [UInt8] = [0]
+    return decode(encoded, cipher: salt)
+  }
+
+  static var cardCreator:String {
+    let encoded: [UInt8] = [0]
+    return decode(encoded, cipher: salt)
+  }
+
+  static var cardCreatorInfo:[String:Any] {
+    return [:]
+  }
+
+  static func decode(_ encoded: [UInt8], cipher: [UInt8]) -> String {
+    var decrypted = [UInt8]()
+
+    return String(bytes: decrypted, encoding: .utf8)!
+  }
+}
