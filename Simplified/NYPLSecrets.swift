14b580e124261f11416a325b55ce3acd2dc31291
Ernest Fan <ernest@Ernests-MacBook-Pro-2.local>
2020-03-25 16:45:59 -0700

    Update NYPLSecrets.swift for multiple vendors structure

--- Simplified/NYPLSecrets.swift
+++ Simplified/NYPLSecrets.swift
@@ -1,24 +1,30 @@
 import Foundation
 
+enum AudioBookVendors: String {
+  case cantook = "cantook"
+}
+
 enum NYPLSecrets {
   private static let salt: [UInt8] = [0]
-
-  static var feedbooks:String {
-    let encoded: [UInt8] = [0]
-    return decode(encoded, cipher: salt)
-  }
-
-  static var feedbooksInfo:[String:Any] {
-    return [:]
-  }
 
   static var cardCreator:String {
     let encoded: [UInt8] = [0]
     return decode(encoded, cipher: salt)
   }
-
+    
   static var cardCreatorInfo:[String:Any] {
     return [:]
+  }
+
+  static func feedbookKeys(forVendor name: AudioBookVendors) -> String {
+    let allKeys : [String: [UInt8]] = [:]
+    guard let encoded = allKeys[name.rawValue] else { return "" }
+    return decode(encoded, cipher: salt)
+  }
+
+  static func feedbookInfo(forVendor name: AudioBookVendors) -> [String:Any] {
+    let info : [String: [String:Any]] = [:]
+    return info[name.rawValue] ?? [:]
   }
 
   static func decode(_ encoded: [UInt8], cipher: [UInt8]) -> String {
