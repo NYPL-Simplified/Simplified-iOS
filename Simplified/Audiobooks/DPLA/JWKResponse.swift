63f51b15ad20fe557f6d82a2f68db300143058b2
Vladimir Fedorov <me@vladimirfedorov.com>
2020-09-11 00:22:11 +0500

    removed an unused JWK field

--- Simplified/Audiobooks/DPLA/JWKResponse.swift
+++ Simplified/Audiobooks/DPLA/JWKResponse.swift
@@ -16,17 +16,13 @@ struct JWKResponse: Codable {
 struct JWK: Codable {
   /// Custom JWK structure in response
   private enum JWKKeys: String, CodingKey {
-    case kty
     case publickKeyEncoded = "http://www.feedbooks.com/audiobooks/signature/pem-key"
   }
-  /// Key type
-  let kty: String
   /// Public key data
   let publicKeyData: Data?
   // Need to manually parse the key with the Url as the key
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: JWKKeys.self)
-    self.kty = try container.decode(String.self, forKey: .kty)
     let pemString = try container.decode(String.self, forKey: .publickKeyEncoded).replacingOccurrences(of: "\n", with: "")
     self.publicKeyData = Data(base64Encoded: RSAUtils.stripPEMKeyHeader(pemString))    
   }
