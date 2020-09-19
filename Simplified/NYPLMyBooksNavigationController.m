ad2c24a0b66bcf1650098a9756cb57a1d72c9602
Ettore Pasquini <ettorepasquini@icloud.com>
2020-09-18 17:35:37 -0700

    Remove code triplication for switchLibrary functionality
    The common "switchLibrary" logic (shared verbatim by the Holds and MyBooks nav controllers, and minimally differently in the Catalog nav controller) is now moved to a common superclass NYPLLibraryNavigationController.
    
    The moved code does not have any changes whatsoever.

--- Simplified/NYPLMyBooksNavigationController.m
+++ Simplified/NYPLMyBooksNavigationController.m
@@ -1,43 +1,24 @@
-#import "NYPLMyBooksViewController.h"
-
-#import "NYPLMyBooksNavigationController.h"
-#import "NYPLBookRegistry.h"
-#import "NYPLConfiguration.h"
-#import "NYPLRootTabBarController.h"
-#import "NYPLCatalogNavigationController.h"
-
-#ifdef SIMPLYE
-// TODO: SIMPLY-3053 this #ifdef can be removed once this ticket is done
-#import "NYPLSettingsPrimaryTableViewController.h"
-#endif
-
 #import "SimplyE-Swift.h"
 
-#if defined(FEATURE_DRM_CONNECTOR)
-#import <ADEPT/ADEPT.h>
-#endif
+#import "NYPLMyBooksViewController.h"
+#import "NYPLMyBooksNavigationController.h"
+
 
 @implementation NYPLMyBooksNavigationController
 
-#pragma mark NSObject
+#pragma mark - NSObject
 
 - (instancetype)init
 {
-  NYPLMyBooksViewController *viewController =
-  [[NYPLMyBooksViewController alloc] init];
+  NYPLMyBooksViewController *vc = [[NYPLMyBooksViewController alloc] init];
   
-  self = [super initWithRootViewController:viewController];
+  self = [super initWithRootViewController:vc];
   if(!self) return nil;
   
   self.tabBarItem.image = [UIImage imageNamed:@"MyBooks"];
   
 #ifdef SIMPLYE
-  viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]
-                                                     initWithImage:[UIImage imageNamed:@"Catalog"] style:(UIBarButtonItemStylePlain)
-                                                     target:self
-                                                     action:@selector(switchLibrary)];
-  viewController.navigationItem.leftBarButtonItem.accessibilityLabel = NSLocalizedString(@"AccessibilitySwitchLibrary", nil);
-  viewController.navigationItem.leftBarButtonItem.enabled = YES;
+  [self setNavigationLeftBarButtonForVC:vc];
 #endif
   
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(currentAccountChanged) name:NSNotification.NYPLCurrentAccountDidChange object:nil];
@@ -50,15 +31,17 @@
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+#pragma mark - UIViewController
+
 -(void)viewWillAppear:(BOOL)animated
 {
   [super viewWillAppear:animated];
     
-  NYPLMyBooksViewController *viewController = (NYPLMyBooksViewController *)self.visibleViewController;
-  
-  viewController.navigationItem.title = [AccountsManager shared].currentAccount.name;
-    
+  UIViewController *visibleVC = self.visibleViewController;
+  visibleVC.navigationItem.title = [AccountsManager shared].currentAccount.name;
 }
+
+#pragma mark - Callbacks
 
 - (void)currentAccountChanged
 {
@@ -70,77 +53,5 @@
     [self popToRootViewControllerAnimated:NO];
   }
 }
-
-#ifdef SIMPLYE
-- (void)switchLibrary
-{
-  UIViewController *viewController = self.visibleViewController;
-
-  UIAlertControllerStyle style;
-  if (viewController && viewController.navigationItem.leftBarButtonItem) {
-    style = UIAlertControllerStyleActionSheet;
-  } else {
-    style = UIAlertControllerStyleAlert;
-  }
-
-  UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"PickYourLibrary", nil) message:nil preferredStyle:style];
-  alert.popoverPresentationController.barButtonItem = viewController.navigationItem.leftBarButtonItem;
-  alert.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp;
-
-  NSArray *accounts = [[NYPLSettings sharedSettings] settingsAccountsList];
-
-  for (int i = 0; i < (int)accounts.count; i++) {
-    Account *account = [[AccountsManager sharedInstance] account:accounts[i]];
-    if (!account) {
-      continue;
-    }
-
-    [alert addAction:[UIAlertAction actionWithTitle:account.name style:(UIAlertActionStyleDefault) handler:^(__unused UIAlertAction *_Nonnull action) {
-
-      BOOL workflowsInProgress;
-#if defined(FEATURE_DRM_CONNECTOR)
-      workflowsInProgress = ([NYPLADEPT sharedInstance].workflowsInProgress || [NYPLBookRegistry sharedRegistry].syncing == YES);
-#else
-      workflowsInProgress = ([NYPLBookRegistry sharedRegistry].syncing == YES);
-#endif
-
-      if (workflowsInProgress) {
-        [self presentViewController:[NYPLAlertUtils
-                                     alertWithTitle:@"PleaseWait"
-                                     message:@"PleaseWaitMessage"]
-                           animated:YES
-                         completion:nil];
-      } else {
-        [[NYPLBookRegistry sharedRegistry] save];
-        [self updateCatalogFeedSettingCurrentAccount:account];
-      }
-    }]];
-  }
-
-  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"ManageAccounts", nil) style:(UIAlertActionStyleDefault) handler:^(__unused UIAlertAction *_Nonnull action) {
-    NSUInteger tabCount = [[[NYPLRootTabBarController sharedController] viewControllers] count];
-    UISplitViewController *splitViewVC = [[[NYPLRootTabBarController sharedController] viewControllers] lastObject];
-    UINavigationController *masterNavVC = [[splitViewVC viewControllers] firstObject];
-    [masterNavVC popToRootViewControllerAnimated:NO];
-    [[NYPLRootTabBarController sharedController] setSelectedIndex:tabCount-1];
-    NYPLSettingsPrimaryTableViewController *tableVC = [[masterNavVC viewControllers] firstObject];
-    [tableVC.delegate settingsPrimaryTableViewController:tableVC didSelectItem:NYPLSettingsPrimaryTableViewControllerItemAccount];
-  }]];
-
-  [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil) style:(UIAlertActionStyleCancel) handler:nil]];
-
-  [[NYPLRootTabBarController sharedController] safelyPresentViewController:alert animated:YES completion:nil];
-}
-
-- (void)updateCatalogFeedSettingCurrentAccount:(Account *)account
-{
-  [AccountsManager shared].currentAccount = account;
-  NYPLCatalogNavigationController * catalog = (NYPLCatalogNavigationController*)[NYPLRootTabBarController sharedController].viewControllers[0];
-  [catalog updateFeedAndRegistryOnAccountChange];
-
-  UIViewController *visibleVC = self.visibleViewController;
-  visibleVC.navigationItem.title = [AccountsManager shared].currentAccount.name;
-}
-#endif
 
 @end
