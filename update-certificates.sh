62261b6e033d35456837821113f81754bc8e1816
Ettore Pasquini <ettorepasquini@icloud.com>
2020-10-01 11:31:52 -0700

    SIMPLY-2662 Set up Crashlytics for Open eBooks

--- update-certificates.sh
+++ update-certificates.sh
@@ -9,6 +9,7 @@ cp ../Certificates/SimplyE/iOS/APIKeys.swift Simplified/
 cp ../Certificates/SimplyE/iOS/ReaderClientCertProduction.sig Simplified/ReaderClientCert.sig
 
 cp ../Certificates/OpenEbooks/iOS/ReaderClientCert.sig Simplified/OpenEbooks/Resources/ReaderClientCert.sig
+cp ../Certificates/OpenEbooks/iOS/GoogleService-Info.plist Simplified/OpenEbooks/Resources/
 
 git update-index --skip-worktree Simplified/NYPLSecrets.swift
 swift ../Certificates/SimplyE/iOS/KeyObfuscator.swift
