e667782186c893d81a5e2b067ff91d5b745e76f7
Ettore Pasquini <ettorepasquini@icloud.com>
2020-12-04 16:13:24 -0800

    Add scripts to build the apps and push binary to ios-binaries
    The script that does all of it is `archive-and-upload-adhoc.sh`
    
    This system of scripts should work for both SimplyE and Open eBooks.

---
+++ scripts/xcode-export-adhoc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# SUMMARY
+#   Exports an archive for SimplyE / Open eBooks and generates the related ipa.
+#
+# SYNOPSIS
+#   xcode-export-adhoc.sh [ simplye | SE | openebooks | OE ]
+#
+# USAGE
+#   Run this script from the root of Simplified-iOS repo, e.g.:
+#
+#     ./scripts/xcode-export-adhoc.sh simplye
+#
+# RESULTS
+#   The generated .ipa is placed in its own directory inside
+#   `./Build/exports-adhoc`.
+
+source "$(dirname $0)/xcode-settings.sh"
+
+echo "Exporting $ARCHIVE_NAME for Ad-Hoc distribution..."
+
+mkdir -p "$ADHOC_EXPORT_PATH"
+
+xcodebuild -archivePath "$ARCHIVE_PATH.xcarchive" \
+            -exportOptionsPlist "$APP_NAME/exportOptions-adhoc.plist" \
+            -exportPath "$ADHOC_EXPORT_PATH" \
+            -allowProvisioningUpdates \
+            -exportArchive #| xcpretty
