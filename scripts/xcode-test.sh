296e4bfc041b97799f891d8ad3c38031c995e229
Ettore Pasquini <ettorepasquini@icloud.com>
2020-12-09 18:43:51 -0800

    Add script to run unit tests

---
+++ scripts/xcode-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SUMMARY
+#   Runs the unit tests for SimplyE / Open eBooks.
+#
+# SYNOPSIS
+#   xcode-test.sh [ simplye | SE | openebooks | OE ]
+#
+# USAGE
+#   Run this script from the root of Simplified-iOS repo, e.g.:
+#
+#     ./scripts/xcode-test.sh simplye
+#
+
+source "$(dirname $0)/xcode-settings.sh"
+
+echo "Running unit tests for $APP_NAME..."
+
+xcodebuild -project $PROJECT_NAME \
+           -scheme $SCHEME \
+           -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11 \
+           clean test
