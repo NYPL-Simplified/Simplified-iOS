54f2b03660fbeb1ebcd76f20f7253eadfca3d9d9
Ettore Pasquini <ettorepasquini@icloud.com>
2021-01-07 14:00:34 -0800

    SIMPLY-3414 Update R2-integration build script

--- scripts/build-carthage-R2-integration.sh
+++ scripts/build-carthage-R2-integration.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Usage: run this script from the root of Simplified-iOS repo.
+# SUMMARY
+#   This scripts wipes your Carthage folder, checks out and rebuilds
+#   all Carthage dependencies for working on R2 integration.
 #
-#     ./scripts/build-carthage-R2.sh
+# SYNOPSIS
+#     ./scripts/build-carthage-R2-integration.sh
 #
-# Description: This scripts wipes your Carthage folder, checks out and rebuilds
-#              all dependencies.
+# USAGE
+#   Run this script from the root of Simplified-iOS repo.
+#   Use this script in conjuction with the SimplifiedR2.workspace. This
+#   assumes that you have the R2 repos checked out as siblings of
+#   Simplified-iOS.
 #
 # NOTES
 #   This is meant to be used locally. It won't work in a GitHub Actions CI
@@ -20,30 +26,30 @@ rm -rf Carthage
 echo "Building r2-shared-swift Carthage dependencies..."
 cd ../r2-shared-swift
 rm -rf Carthage
-carthage update --platform iOS
+carthage build --platform iOS
 
 echo "Building r2-lcp-swift Carthage dependencies..."
 cd ../r2-lcp-swift
 rm -rf Carthage
 swift ../Certificates/SimplyE/iOS/LCPLib.swift -f
-carthage update --platform iOS
+carthage build --platform iOS
 
 echo "Building r2-streamer-swift Carthage dependencies..."
 cd ../r2-streamer-swift
 rm -rf Carthage
-carthage update --platform iOS
+carthage build --platform iOS
 
 echo "Building r2-navigator-swift Carthage dependencies..."
 cd ../r2-navigator-swift
 rm -rf Carthage
-carthage update --platform iOS
+carthage build --platform iOS
 
 echo "Done with R2 Carthage dependencies."
 cd ..
 
 # remove R2 dependencies from Carthage since we'll build them in the R2 workspace
 sed -i '' "s|github \"NYPL-Simplified/r2|#github \"NYPL-Simplified/r2|" Cartfile
-sed -i '' "s|github \"NYPL-Simplified/r2||" Cartfile.resolved
+sed -i '' "s|github \"NYPL-Simplified/r2.*||" Cartfile.resolved
 
 carthage checkout
 ./Carthage/Checkouts/NYPLAEToolkit/scripts/fetch-audioengine.sh
