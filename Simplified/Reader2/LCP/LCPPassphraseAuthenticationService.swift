20fe8d086223d0bd125f557ad15fdef91241fcad
Ernest Fan <ernestf@evidentpoint.com>
2021-02-11 14:57:39 -0800

    Add documentation and logging

--- Simplified/Reader2/LCP/LCPPassphraseAuthenticationService.swift
+++ Simplified/Reader2/LCP/LCPPassphraseAuthenticationService.swift
@@ -11,6 +11,9 @@
 import Foundation
 import ReadiumLCP
 
+/**
+ For Passphrase in License Document, see https://readium.org/lcp-specs/releases/lcp/latest#41-introduction
+ */
 class LCPPassphraseAuthenticationService: LCPAuthenticating {
   func retrievePassphrase(for license: LCPAuthenticatedLicense, reason: LCPAuthenticationReason, allowUserInteraction: Bool, sender: Any?, completion: @escaping (String?) -> Void) {
     guard let hintLink = license.hintLink,
@@ -23,19 +26,27 @@ class LCPPassphraseAuthenticationService: LCPAuthenticating {
     NYPLNetworkExecutor.shared.GET(hintURL) { (result) in
       switch result {
       case .success(let data, _):
-        if let json = try? JSONSerialization.jsonObject(with: data, options: []) as! [String:Any],
-          let passphrase = json["passphrase"] as? String {
+        if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any],
+          let passphrase = json?["passphrase"] as? String {
           completion(passphrase)
         } else {
-          Log.error(#file, "Error parsing JSON or finding passphrase key/value.")
+          let responseBody = String(data: data, encoding: .utf8)
+          NYPLErrorLogger.logError(
+            withCode: .lcpPassphraseAuthorizationFail,
+            summary: "LCP Passphrase JSON Parse Error",
+            metadata: [
+              "hintUrl": hintURL,
+              "responseBody": responseBody ?? "N/A",
+          ])
           completion(nil)
         }
       case .failure(let error, _):
         NYPLErrorLogger.logError(
           withCode: .lcpPassphraseAuthorizationFail,
-          summary: "Unable to retrieve LCP passphrase from \(hintURL)",
+          summary: "Unable to retrieve LCP passphrase",
           metadata: [
             NSUnderlyingErrorKey: error,
+            "hintUrl": hintURL,
         ])
         completion(nil)
       }
