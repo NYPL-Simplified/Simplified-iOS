6cbfe76e92457d3c9f93b75b30afb249fd417db8
Ernest Fan <ernestf@evidentpoint.com>
2021-04-08 20:05:16 -0700

    Use localizable.stringsdict for plural handling

--- Simplified/Utilities/Date-Time/Date+NYPLAdditions.swift
+++ Simplified/Utilities/Date-Time/Date+NYPLAdditions.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum NYPLDateType {
+enum NYPLDateType: String {
   case year
   case month
   case week
@@ -55,49 +55,26 @@ public extension Date {
     
     if(years >= 4) {
       // Switch to years after ~48 months.
-      return "\(Int(years)) " + dateSuffix(dateType: .year, suffixType: suffixType, isPlural: true)
+      return String.localizedStringWithFormat(dateSuffix(dateType: .year, suffixType: suffixType, isPlural: true), Int(years))
     } else if(months >= 4) {
       // Switch to months after ~16 weeks.
-      return "\(Int(months)) " + dateSuffix(dateType: .month, suffixType: suffixType, isPlural: true)
+      return String.localizedStringWithFormat(dateSuffix(dateType: .month, suffixType: suffixType, isPlural: true), Int(months))
     } else if(weeks >= 4) {
       // Switch to weeks after 28 days.
-      return "\(Int(weeks)) " + dateSuffix(dateType: .week, suffixType: suffixType, isPlural: true)
+      return String.localizedStringWithFormat(dateSuffix(dateType: .week, suffixType: suffixType, isPlural: true), Int(weeks))
     } else if(days >= 2) {
       // Switch to days after 48 hours.
-      return "\(Int(days)) " + dateSuffix(dateType: .day, suffixType: suffixType, isPlural: true)
+      return String.localizedStringWithFormat(dateSuffix(dateType: .day, suffixType: suffixType, isPlural: true), Int(days))
     } else {
       // Use hours.
-      return "\(Int(hours)) " + dateSuffix(dateType: .hour, suffixType: suffixType, isPlural: hours != 1)
+      return String.localizedStringWithFormat(dateSuffix(dateType: .hour, suffixType: suffixType, isPlural: hours != 1), Int(hours))
     }
   }
   
   private func dateSuffix(dateType: NYPLDateType, suffixType: NYPLDateSuffixType, isPlural: Bool) -> String {
     if suffixType == .short {
-      switch dateType {
-      case .year:
-        return NSLocalizedString("year_suffix_short", comment: "Year (Date Suffix)")
-      case .month:
-        return NSLocalizedString("month_suffix_short", comment: "Month (Date Suffix)")
-      case .week:
-        return NSLocalizedString("week_suffix_short", comment: "Week (Date Suffix)")
-      case .day:
-        return NSLocalizedString("day_suffix_short", comment: "Day (Date Suffix)")
-      default:
-        return NSLocalizedString("hour_suffix_short", comment: "Hour (Date Suffix)")
-      }
+      return NSLocalizedString("\(dateType.rawValue)_suffix_short", comment: "Date Suffix (Short)")
     }
-    
-    switch dateType {
-    case .year:
-      return NSLocalizedString(isPlural ? "year_suffix_plural" : "year_suffix_long", comment: "Year (Date Suffix)")
-    case .month:
-      return NSLocalizedString(isPlural ? "month_suffix_plural" : "month_suffix_long", comment: "Month (Date Suffix)")
-    case .week:
-      return NSLocalizedString(isPlural ? "week_suffix_plural" : "week_suffix_long", comment: "Week (Date Suffix)")
-    case .day:
-      return NSLocalizedString(isPlural ? "day_suffix_plural" : "day_suffix_long", comment: "Day (Date Suffix)")
-    default:
-      return NSLocalizedString(isPlural ? "hour_suffix_plural" : "hour_suffix_long", comment: "Hour (Date Suffix)")
-    }
+    return NSLocalizedString("\(dateType.rawValue)_suffix_long", comment: "Date Suffix (Long)")
   }
 }
