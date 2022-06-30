d7aa98d199f876496b315bc4f64ed4fbd3383b24
Ettore Pasquini <ettorepasquini@icloud.com>
2022-06-29 23:57:25 -0700

    OE-514 Create CleverHelper using library Account

--- Simplified/SignInLogic/OELoginCleverHelper.swift
+++ Simplified/SignInLogic/OELoginCleverHelper.swift
@@ -10,22 +10,20 @@ import UIKit
 
 class OELoginCleverHelper: NSObject {
 
-  var signInBusinessLogic: NYPLSignInBusinessLogic!
-  weak var navigationController: UINavigationController?
+  private(set) var signInBusinessLogic: NYPLSignInBusinessLogic!
+  private weak var navigationController: UINavigationController?
   var forceEditability: Bool = false
-  weak var postLoginConfigurator: OEAppUIStructureConfigurating?
+  private weak var postLoginConfigurator: OEAppUIStructureConfigurating?
 
-  init(navigationController: UINavigationController,
+  init(libraryAccount: Account,
        postLoginConfigurator: OEAppUIStructureConfigurating?) {
-    self.navigationController = navigationController
     self.postLoginConfigurator = postLoginConfigurator
 
     super.init()
 
-    let accountManager = AccountsManager.shared
     self.signInBusinessLogic = NYPLSignInBusinessLogic(
-      libraryAccountID: accountManager.currentAccountId!, //TODO
-      libraryAccountsProvider: accountManager,
+      libraryAccountID: libraryAccount.uuid,
+      libraryAccountsProvider: AccountsManager.shared,
       urlSettingsProvider: NYPLSettings.shared,
       bookRegistry: NYPLBookRegistry.shared(),
       bookDownloadsRemover: NYPLMyBooksDownloadCenter.shared(),
@@ -35,7 +33,8 @@ class OELoginCleverHelper: NSObject {
       drmAuthorizerAxis: NYPLAxisDRMAuthorizer.sharedInstance)
   }
 
-  func startCleverFlow() {
+  func startCleverFlow(onNavigationController navController: UINavigationController?) {
+    navigationController = navController
     let cleverAuth = signInBusinessLogic.libraryAccount?.details?.auths.filter { auth in
       auth.isOauthIntermediary
     }.first
