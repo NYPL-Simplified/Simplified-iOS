a6ecf6089599b1325588a5cbb354444fca984bc7
Ettore Pasquini <ettorepasquini@icloud.com>
2022-08-01 13:34:17 -0700

    IOS-415 Use sha256 api from NYPLUtilities
    And remove duplicated code.

--- Simplified/Utilities/Strings/NSString+NYPLStringAdditions.h
+++ Simplified/Utilities/Strings/NSString+NYPLStringAdditions.h
@@ -4,8 +4,6 @@
 
 - (NSString *)fileSystemSafeBase64EncodedStringUsingEncoding:(NSStringEncoding)encoding;
 
-- (NSString *)SHA256;
-
 /**
  @returns A string made with the assumption that the receiver is a query
  param value.
--- Simplified/Utilities/Strings/NSString+NYPLStringAdditions.m
+++ Simplified/Utilities/Strings/NSString+NYPLStringAdditions.m
@@ -1,5 +1,7 @@
 #import <CommonCrypto/CommonDigest.h>
 
+@import NYPLUtilities;
+
 #import "NSString+NYPLStringAdditions.h"
 
 @implementation NSString (NYPLStringAdditions)
@@ -32,28 +34,6 @@
            stringByReplacingOccurrencesOfString:@"+" withString:@"-"]
           stringByReplacingOccurrencesOfString:@"/" withString:@"_"];
 #pragma clang diagnostic pop
-}
-
-- (NSString *)SHA256
-{
-  NSData *const input = [self dataUsingEncoding:NSUTF8StringEncoding];
-  unsigned char output[CC_SHA256_DIGEST_LENGTH];
-
-  CC_SHA256(input.bytes, (CC_LONG)input.length, output);
-  
-  char s[CC_SHA256_DIGEST_LENGTH * 2 + 1];
-  s[CC_SHA256_DIGEST_LENGTH * 2] = '\0';
-  
-  const char *const hex = "0123456789abcdef";
-  
-  for(unsigned int i = 0; i < CC_SHA256_DIGEST_LENGTH; ++i) {
-    s[i * 2] = hex[output[i] / 16];
-    s[i * 2 + 1] = hex[output[i] % 16];
-  }
-  
-  return [[NSString alloc] initWithBytes:s
-                                  length:(CC_SHA256_DIGEST_LENGTH * 2)
-                                encoding:NSASCIIStringEncoding];
 }
 
 - (NSString *)stringURLEncodedAsQueryParamValue
