42f10177ad26fcb543c4ef0af87c2323bbd564b5
Ernest Fan <ernestf@evidentpoint.com>
2022-11-01 15:17:03 -0700

    Update testflight upload script to include team id

--- scripts/testflight-upload.sh
+++ scripts/testflight-upload.sh
@@ -16,6 +16,7 @@
 
 source "$(dirname $0)/xcode-settings.sh"
 
+TEAM_ID="7262U6ST2R"
 IPA_PATH="$APPSTORE_EXPORT_PATH/$APP_NAME.ipa"
 echo "Uploading $IPA_PATH to TestFlight..."
 
@@ -25,6 +26,7 @@ if [ "$BUILD_CONTEXT" == "ci" ]; then
 fi
 
 fastlane deliver --ipa "$IPA_PATH" \
+  --itc_provider "$TEAM_ID" \
   --skip_screenshots --skip_metadata --skip_app_version_update \
   --precheck_include_in_app_purchases false \
   --force
