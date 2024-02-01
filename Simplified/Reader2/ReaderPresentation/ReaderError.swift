7ac7b9a678e5e3f7436eb9653a1f9109f914446d
Ettore Pasquini <ettorepasquini@icloud.com>
2024-02-01 15:46:09 -0800

    IOS-631 Fix crash when Publication::baseURL is nil

--- Simplified/Reader2/ReaderPresentation/ReaderError.swift
+++ Simplified/Reader2/ReaderPresentation/ReaderError.swift
@@ -14,15 +14,16 @@ import Foundation
 
 enum ReaderError: LocalizedError {
   case formatNotSupported
-  case epubNotValid
-  
+  case epubNotValid(String)
+
   var errorDescription: String? {
     switch self {
     case .formatNotSupported:
       return NSLocalizedString("The book you were trying to read is in an unsupported format.", comment: "Error message when trying to read a publication with a unsupported format")
-    case .epubNotValid:
-      return NSLocalizedString("The book you were trying to read is corrupted. Please try downloading it again.", comment: "Error message when trying to read an EPUB that is invalid")
+    case .epubNotValid(let errorCause):
+      return String.localizedStringWithFormat(
+        NSLocalizedString("The book you were trying to read is corrupted (%@). Please try downloading it again.", comment: "Error message when trying to read an EPUB that is invalid"),
+        errorCause)
     }
   }
-  
 }
